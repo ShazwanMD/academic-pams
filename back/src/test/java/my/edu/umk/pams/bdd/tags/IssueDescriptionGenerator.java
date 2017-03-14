2b268bb6627babfb333f593c08fdda1ef19c956d
max.zain <max.zain@canang.com.my>
2017-03-14 11:11:57 +0800

    add jgiven bdd tags

---
+++ back/src/test/java/my/edu/umk/pams/bdd/tags/IssueDescriptionGenerator.java
@@ -0,0 +1,19 @@
+package my.edu.umk.pams.bdd.tags;
+
+import com.tngtech.jgiven.annotation.TagDescriptionGenerator;
+import com.tngtech.jgiven.config.TagConfiguration;
+
+import java.lang.annotation.Annotation;
+
+/**
+ * Created by PAMS on 3/11/2017.
+ */
+public class IssueDescriptionGenerator implements TagDescriptionGenerator {
+    @Override
+    public String generateDescription(TagConfiguration tagConfiguration,
+                                      Annotation annotation, Object value ) {
+        return String.format(
+                "<a href='http://jira.umk.edu.my:8080/PAMI/issues/%s'>Issue %s</a>",
+                value, value );
+    }
+}
