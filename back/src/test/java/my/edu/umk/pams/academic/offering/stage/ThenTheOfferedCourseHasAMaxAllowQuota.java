1630f7ffcb569f5b45c095b4530bdc998130b328
asyikinresli <asyikinresli@10.3.32.62>
2017-03-14 12:36:00 +0800

    commit

--- back/src/test/java/my/edu/umk/pams/academic/offering/stage/ThenTheOfferedCourseHasAMaxAllowQuota.java
+++ back/src/test/java/my/edu/umk/pams/academic/offering/stage/ThenTheOfferedCourseHasAMaxAllowQuota.java
@@ -1,13 +1,37 @@
-package my.edu.umk.pams.academic.offering.stage;
 /**
  * @author asyikin.mr@umk and ziana
+
  */
+
+package my.edu.umk.pams.academic.offering.stage;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 
-public class ThenTheOfferedCourseHasAMaxAllowQuota  extends Stage<ThenTheOfferedCourseHasAMaxAllowQuota>{
+import io.jsonwebtoken.lang.Assert;
+import my.edu.umk.pams.academic.identity.model.AdAddress;
+import my.edu.umk.pams.academic.studyplan.model.AdCourse;
+import my.edu.umk.pams.academic.studyplan.service.StudyplanService;
 
-	 public ThenTheOfferedCourseHasAMaxAllowQuota the_offered_course_has_a_maximum_quota() {
-	       
-		 return self();
-	    }
+public class ThenTheOfferedCourseHasAMaxAllowQuota extends Stage<ThenTheOfferedCourseHasAMaxAllowQuota> {
+
+	private static final Logger LOG = LoggerFactory.getLogger(ThenTheOfferedCourseHasAMaxAllowQuota.class);
+
+	@Autowired
+	private StudyplanService studyplanService;
+
+	@ExpectedScenarioState
+	private AdCourse course;
+
+	public ThenTheOfferedCourseHasAMaxAllowQuota the_offered_course_has_a_maximum_quota() {
+		 List<AdCourse> courses = studyplanService.findCourseByCode(code);
+	        Assert.notEmpty(courses);
+		
+	        return self();
+		
+	}
 }
