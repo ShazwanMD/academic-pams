4570afe5402a0554990b25c14ff79330e577c5cf
ziana <ziana@10.3.32.209>
2017-03-16 16:30:03 +0800

    cOMMIT WHEN N THEN

---
+++ back/src/test/java/my/edu/umk/pams/academic/studyplan/stage/ThenCanTakeInNewSemester.java
@@ -0,0 +1,13 @@
+package my.edu.umk.pams.academic.studyplan.stage;
+
+import com.tngtech.jgiven.integration.spring.JGivenStage;
+
+@JGivenStage
+public class ThenCanTakeInNewSemester {
+
+	public void Can_take_in_new_semester() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ back/src/test/java/my/edu/umk/pams/academic/studyplan/stage/WhenIWantToViewTheProgramForEachLevelOfStudy.java
@@ -0,0 +1,17 @@
+package my.edu.umk.pams.academic.studyplan.stage;
+
+import com.tngtech.jgiven.integration.spring.JGivenStage;
+
+@JGivenStage
+public class WhenIWantToViewTheProgramForEachLevelOfStudy {
+
+	public void I_want_to_view_the_program_for_each_level_of_study_$(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	
+	
+	
+}
