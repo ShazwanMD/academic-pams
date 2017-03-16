d308d84ae943278451c8309f6229c239d8a5f0b9
rafizan_baharum <rafizan.baharum@canang.com.my>
2017-03-16 10:29:28 +0800

    Add frontend

---
+++ front/src/services/index.ts
@@ -0,0 +1,7 @@
+export { StudyplanService } from './studyplan.service';
+export { OfferingService } from './offering.service';
+export { AssessmentService } from './assessment.service';
+export { GraduationService } from './graduation.service';
+export { IdentityService } from './identity.service';
+export { SystemService } from './system.service';
+export { CommonService } from './common.service';
