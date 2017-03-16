0d27e05105f66e4675a93ece5af65eb09b594dbe
ziana <ziana@10.3.32.209>
2017-03-16 16:29:13 +0800

    add US_AD_SPL_2001 BY ZAIDA_AIN

---
+++ back/src/test/java/my/edu/umk/pams/academic/studyplan/US_AD_SPL_2001.java
@@ -0,0 +1,62 @@
+package my.edu.umk.pams.academic.studyplan;
+
+/**
+ * As an admin academic,
+ *  I am a student in current academic session,
+ *      Can take in new semester
+ *
+ * @author zaida_ain
+ */
+
+import org.slf4j.Logger;
+
+import org.slf4j.LoggerFactory;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.tngtech.jgiven.integration.spring.JGivenStage;
+import com.tngtech.jgiven.integration.spring.SpringScenarioTest;
+
+import my.edu.umk.pams.academic.config.TestAppConfiguration;
+import my.edu.umk.pams.academic.studyplan.stage.ThenCanTakeInNewSemester;
+import my.edu.umk.pams.academic.studyplan.stage.WhenIWantToViewTheProgramForEachLevelOfStudy;
+import my.edu.umk.pams.bdd.stage.GivenIAmStudent;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+@ContextConfiguration(classes = TestAppConfiguration.class)
+
+public class US_AD_SPL_2001 extends SpringScenarioTest<GivenIAmStudent, WhenIWantToViewTheProgramForEachLevelOfStudy, ThenCanTakeInNewSemester> {
+
+	
+	@SuppressWarnings("unused")
+	private static final Logger LOG = LoggerFactory.getLogger(US_AD_SPL_2001.class);
+
+	@Before
+	public void before() {
+	}
+
+	@After
+	public void after() {
+	}
+
+	@Test
+	@Rollback(true)
+	public void scenari2001() {
+		given().I_am_a_student_in_current_academic_session();
+		when().I_want_to_view_the_program_for_each_level_of_study_$("45");
+		then().Can_take_in_new_semester();
+	}    
+
+}
+	
+	
+
+
