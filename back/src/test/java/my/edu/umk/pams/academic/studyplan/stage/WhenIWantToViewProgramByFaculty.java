69ef50b3695243785c04145f19844d4e0bd07c17
ain <ain@10.3.32.66>
2017-03-20 11:21:36 +0800

    Add file US_AD_SPL_2002

---
+++ back/src/test/java/my/edu/umk/pams/academic/studyplan/stage/WhenIWantToViewProgramByFaculty.java
@@ -0,0 +1,11 @@
+package my.edu.umk.pams.academic.studyplan.stage;
+
+public class WhenIWantToViewTheCourseForMyProgram {
+
+
+	public void I_Want_To_View_The_Course_For_My_Program_$(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
