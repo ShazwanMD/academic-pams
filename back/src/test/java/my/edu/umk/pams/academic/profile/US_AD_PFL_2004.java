7827928a7c3937406a329afd25447d620fc16f40
SaM <SaM@10.3.32.182>
2017-03-20 15:55:19 +0800

    add view for US_AD_PFL_2004

---
+++ back/src/test/java/my/edu/umk/pams/academic/profile/US_AD_PFL_2004.java
@@ -0,0 +1,55 @@
+package my.edu.umk.pams.academic.profile;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.tngtech.jgiven.integration.spring.SpringScenarioTest;
+
+import my.edu.umk.pams.academic.config.TestAppConfiguration;
+import my.edu.umk.pams.academic.profile.stage.ThenStudentCourseInfoIsCurrent;
+import my.edu.umk.pams.academic.profile.stage.WhenIWantToViewStudentCourseInfo;
+import my.edu.umk.pams.bdd.stage.GivenIAmAdministrator;
+
+/**
+ * 
+ * 
+ * As an admin, 
+ * I want to view course info, 
+ * so that course info is current
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+@ContextConfiguration(classes = TestAppConfiguration.class)
+public class US_AD_PFL_2004 extends SpringScenarioTest<GivenIAmAdministrator, WhenIWantToViewStudentCourseInfo, ThenStudentCourseInfoIsCurrent>{
+
+	private static final Logger LOG = LoggerFactory.getLogger(US_AD_PFL_2004.class);
+	
+	private static final String FACULTY_NO = "45";
+	
+	@Before
+	public void before() {
+	}
+
+	@After
+	public void after() {
+	}
+	
+	@Test
+	@Rollback(true)
+	public void scenario1006(){
+		
+		given().I_am_a_PPS_administrator_in_current_academic_session();
+		when(). I_view_Student_course_$_info(FACULTY_NO);
+		then().Student_course_info_is_current();
+	}
+	
+}
