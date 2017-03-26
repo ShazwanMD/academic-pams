a314c199e3bff4a523d718918750d29fba84bbbe
asyikinresli <asyikinresli@10.3.32.62>
2017-03-26 11:00:51 +0800

    create new file US_AD_TRM_1010 and US_AD_TRM_1012

---
+++ back/src/test/java/my/edu/umk/pams/academic/term/stage/ThenTheDetailsOfferedCoursesInfoIsCurrent.java
@@ -0,0 +1,33 @@
+package my.edu.umk.pams.academic.term.stage;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
+import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.ExpectedScenarioState;
+import com.tngtech.jgiven.integration.spring.JGivenStage;
+import my.edu.umk.pams.academic.term.model.AdOffering;
+import my.edu.umk.pams.academic.term.service.TermService;
+import my.edu.umk.pams.academic.studyplan.model.AdProgram;
+
+@JGivenStage
+public class ThenTheDetailsOfferedCoursesInfoIsCurrent extends Stage<ThenTheDetailsOfferedCoursesInfoIsCurrent> {
+
+	@Autowired
+	private TermService termService;
+
+	@ExpectedScenarioState
+	private List<AdOffering> offering;
+	
+	@ExpectedScenarioState
+	private AdProgram program;
+	
+	public ThenTheDetailsOfferedCoursesInfoIsCurrent the_details_offered_courses_info_is_current() {
+	
+List<AdOffering> offering = termService.findOfferings(program);
+		
+		Assert.notNull(offering, "Not Null");
+		 
+		return self();
+	}
+}
