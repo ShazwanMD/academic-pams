9d829b67c82f8eff78b825d37b2e142528da60aa
ain <ain@10.3.32.66>
2017-03-28 16:56:23 +0800

    updated ThenAcamedicSessions

--- back/src/test/java/my/edu/umk/pams/academic/planner/stage/ThenAcademicSessionUpdated.java
+++ back/src/test/java/my/edu/umk/pams/academic/planner/stage/ThenAcademicSessionUpdated.java
@@ -6,13 +6,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.Assert;
-
 import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 import com.tngtech.jgiven.integration.spring.JGivenStage;
-
 import my.edu.umk.pams.academic.planner.model.AdAcademicSession;
-import my.edu.umk.pams.academic.planner.model.AdCourse;
 import my.edu.umk.pams.academic.planner.service.PlannerService;
 
 @JGivenStage
