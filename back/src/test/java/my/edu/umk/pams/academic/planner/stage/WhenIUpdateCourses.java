adc90da13ea5ef660cffc8453670107620569727
UMK <UMK@00225B>
2017-03-28 11:01:19 +0800

    ..

--- back/src/test/java/my/edu/umk/pams/academic/planner/stage/WhenIUpdateCourses.java
+++ back/src/test/java/my/edu/umk/pams/academic/planner/stage/WhenIUpdateCourses.java
@@ -1,13 +1,16 @@
 package my.edu.umk.pams.academic.planner.stage;
 
 import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 import com.tngtech.jgiven.annotation.Pending;
 import com.tngtech.jgiven.annotation.ProvidedScenarioState;
 import com.tngtech.jgiven.integration.spring.JGivenStage;
 import my.edu.umk.pams.academic.common.service.CommonService;
 import my.edu.umk.pams.academic.planner.model.AdCourse;
+import my.edu.umk.pams.academic.planner.model.AdCourseImpl;
 import my.edu.umk.pams.academic.planner.model.AdFaculty;
 import my.edu.umk.pams.academic.planner.service.PlannerService;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,12 +31,20 @@ public class WhenIUpdateCourses extends Stage<WhenIUpdateCourses> {
 
     @ProvidedScenarioState
     private AdFaculty faculty;
-
-    @ProvidedScenarioState
+   
+    //@ProvidedScenarioState
+    //private AdCourse course;
+    
+    @ExpectedScenarioState
     private AdCourse course;
+    
 
     @Pending
     public WhenIUpdateCourses I_update_courses_to_faculty_code_$a(String code) {
+    	
+        AdCourse course = new AdCourseImpl();
+        course.setFaculty(plannerService.findFacultyByCode(code));
+        course.setCode("GST8013");
 
         return self();
     }
