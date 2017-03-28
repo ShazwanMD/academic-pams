a92bf0c6e56bfe96af2033fd7be3fb110b342459
abiqsaidi <user@10.3.32.76>
2017-03-28 10:24:41 +0800

    Add view method and remove unused imports

--- back/src/test/java/my/edu/umk/pams/academic/profile/stage/ThenIKnowMyCurrentStatus.java
+++ back/src/test/java/my/edu/umk/pams/academic/profile/stage/ThenIKnowMyCurrentStatus.java
@@ -1,31 +1,42 @@
 package my.edu.umk.pams.academic.profile.stage;
 
 import com.tngtech.jgiven.Stage;
+
 import com.tngtech.jgiven.annotation.ExpectedScenarioState;
-import com.tngtech.jgiven.annotation.Pending;
 import com.tngtech.jgiven.integration.spring.JGivenStage;
+
+import io.jsonwebtoken.lang.Assert;
+import my.edu.umk.pams.academic.identity.model.AdActor;
 import my.edu.umk.pams.academic.identity.model.AdStudent;
+import my.edu.umk.pams.academic.identity.model.AdStudentStatus;
 import my.edu.umk.pams.academic.identity.service.IdentityService;
-import my.edu.umk.pams.academic.profile.service.ProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @JGivenStage
 public class ThenIKnowMyCurrentStatus extends Stage<ThenIKnowMyCurrentStatus> {
 
     @Autowired
-    private ProfileService profileService;
-
-    @Autowired
     private IdentityService identityService;
-
+	
+    @ExpectedScenarioState
+	private AdActor actor;
 
     @ExpectedScenarioState
     private AdStudent student;
+    
+	@ExpectedScenarioState
+	private AdStudentStatus studentStatus;
 
-    @Pending
     public ThenIKnowMyCurrentStatus I_know_my_current_status() {
-        //List<AdStatus> statuses = profileService.findStatus(student);
-        //Assert.notEmpty(statuses);
+
+    	String studentNo = "A17P001";
+    	
+    	AdStudent student = identityService.findStudentByStudentNo(studentNo);
+    	
+    	AdStudentStatus studentStatus = student.getStudentStatus();
+    	
+    	Assert.isTrue(studentStatus.equals(studentStatus), "true");
+    	
         return self();
     }
 
---
+++ back/src/test/java/my/edu/umk/pams/academic/profile/stage/WhenIViewStudentStatus.java
@@ -0,0 +1,42 @@
+package my.edu.umk.pams.academic.profile.stage;
+
+import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.Pending;
+import com.tngtech.jgiven.annotation.ProvidedScenarioState;
+import com.tngtech.jgiven.integration.spring.JGivenStage;
+import my.edu.umk.pams.academic.identity.model.AdStudent;
+import my.edu.umk.pams.academic.identity.service.IdentityService;
+import my.edu.umk.pams.academic.profile.service.ProfileService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@JGivenStage
+public class WhenIViewStudentActivationStatus extends Stage<WhenIViewStudentActivationStatus> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(WhenIUpdateMyBillingAddress.class);
+
+    @Autowired
+    private ProfileService profileService;
+
+    @Autowired
+    private IdentityService identityService;
+
+    @ProvidedScenarioState
+    private AdStudent student;
+
+    @Pending
+    public WhenIViewStudentActivationStatus I_view_student_activation_status() {
+
+        //AdStatus status= new AdStatusImpl();
+
+        //status.setType(AdStatusType.BILLING);
+
+
+        return self();
+    }
+
+
+}
+
+
