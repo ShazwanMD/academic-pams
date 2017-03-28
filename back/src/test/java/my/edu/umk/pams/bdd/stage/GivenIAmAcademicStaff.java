a00c56ba9e6bbd91d6a1953a22bea5370c0bcc2f
rafizan_baharum <rafizan.baharum@canang.com.my>
2017-03-28 21:22:41 +0800

    Fix lecturer username

---
+++ back/src/test/java/my/edu/umk/pams/bdd/stage/GivenIAmAcademicStaff.java
@@ -0,0 +1,87 @@
+package my.edu.umk.pams.bdd.stage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.ProvidedScenarioState;
+import com.tngtech.jgiven.integration.spring.JGivenStage;
+
+import my.edu.umk.pams.academic.identity.model.AdActor;
+import my.edu.umk.pams.academic.identity.model.AdUser;
+import my.edu.umk.pams.academic.planner.model.AdAcademicSession;
+import my.edu.umk.pams.academic.planner.model.AdProgram;
+import my.edu.umk.pams.academic.planner.service.PlannerService;
+import my.edu.umk.pams.academic.security.integration.AdUserDetails;
+import my.edu.umk.pams.academic.term.model.AdOffering;
+import my.edu.umk.pams.academic.term.model.AdSection;
+import my.edu.umk.pams.academic.term.service.TermService;
+
+@JGivenStage
+public class GivenIAmAcademicStaff extends Stage<GivenIAmAcademicStaff> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(GivenIAmAcademicStaff.class);
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private PlannerService plannerService;
+
+    @Autowired
+    private TermService termService;
+
+    @ProvidedScenarioState
+    private AdAcademicSession academicSession;
+
+    @ProvidedScenarioState
+    private AdProgram program;
+
+    @ProvidedScenarioState
+    private AdOffering offering;
+
+    @ProvidedScenarioState
+    private AdSection section;
+
+    private AdActor staff;
+
+    public GivenIAmAcademicStaff I_am_a_staff_in_$_academic_session(String academicSessionCode) {
+        loginAsStaff();
+        academicSession = plannerService.findAcademicSessionByCode(academicSessionCode);
+        return self();
+    }
+
+    public GivenIAmAcademicStaff I_am_a_staff_in_current_academic_session() {
+        loginAsStaff();
+        academicSession = plannerService.findCurrentAcademicSession();
+        return self();
+    }
+
+    public GivenIAmAcademicStaff I_pick_program_$(String code) {
+        program = plannerService.findProgramByCode(code);
+        return self();
+    }
+
+    public GivenIAmAcademicStaff I_pick_offering_$(String canonicalCode) {
+        offering = termService.findOfferingByCanonicalCode(canonicalCode);
+        return self();
+    }
+
+    public GivenIAmAcademicStaff I_pick_section_$(String canonicalCode) {
+        section = termService.findSectionByCanonicalCode(canonicalCode);
+        return self();
+    }
+
+    private void loginAsStaff() {
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken("lecturer1", "abc123");
+        Authentication authed = authenticationManager.authenticate(token);
+        SecurityContextHolder.getContext().setAuthentication(authed);
+        AdUser user = ((AdUserDetails) authed.getPrincipal()).getUser();
+        staff = (AdActor) user.getActor();
+    }
+}
