172e85471360ceeee3bd7bf567fc61f5a392732f
UMK <UMK@00225B>
2017-03-28 15:26:17 +0800

    create ThenIKnowStudentCurrentStatus

---
+++ back/src/test/java/my/edu/umk/pams/academic/profile/stage/ThenIKnowStudentCurrentStatus.java
@@ -0,0 +1,10 @@
+package my.edu.umk.pams.academic.profile.stage;
+
+public class ThenIKnowStudentCurrentStatus {
+
+	public void I_know_student_current_status() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
