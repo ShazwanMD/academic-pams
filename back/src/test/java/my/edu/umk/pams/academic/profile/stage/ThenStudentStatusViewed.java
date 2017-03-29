dec4dbf0d7ce01199c11577c31067420c75aa468
PTM <PTM@PTM-PC>
2017-03-29 13:31:23 +0800

    up

--- back/src/test/java/my/edu/umk/pams/academic/profile/stage/ThenStudentStatusViewed.java
+++ back/src/test/java/my/edu/umk/pams/academic/profile/stage/ThenStudentStatusViewed.java
@@ -1,7 +1,10 @@
 package my.edu.umk.pams.academic.profile.stage;
 
-public class ThenStudentStatusViewed {
+import com.tngtech.jgiven.annotation.Pending;
 
+public class ThenStudentStatusViewed {
+	
+	@Pending
 	public void I_know_student_status_current() {
 		// TODO action to Ihsan
 		
--- back/src/test/java/my/edu/umk/pams/academic/profile/stage/WhenReviewStudentStatus.java
+++ back/src/test/java/my/edu/umk/pams/academic/profile/stage/WhenReviewStudentStatus.java
@@ -1,7 +1,10 @@
 package my.edu.umk.pams.academic.profile.stage;
 
-public class WhenReviewStudentStatus {
+import com.tngtech.jgiven.annotation.Pending;
 
+public class WhenReviewStudentStatus {
+	
+	@Pending
 	public void I_view_student_current_status_for_$(String identityNo) {
 		// TODO action to Ihsan
 		
