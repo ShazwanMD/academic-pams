01056b6956f9a13ed9f9aac1d7f10f6afb8759d0
Zaida Nawi <zaida.n@umk.edu.my>
2017-03-29 09:29:06 +0800

    Udate Then US_AD_PNR_1006

--- back/src/test/java/my/edu/umk/pams/academic/planner/stage/ThenICanManageScheduleByProgram.java
+++ back/src/test/java/my/edu/umk/pams/academic/planner/stage/ThenICanManageScheduleByProgram.java
@@ -11,27 +11,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 public class ThenICanManageScheduleByProgram extends Stage<ThenICanManageScheduleByProgram> {
 
-    @Autowired
-    private PlannerService plannerService;
+	@Autowired
+	private PlannerService plannerService;
 
-    @ExpectedScenarioState
-    private AdStudent student;
+	@ExpectedScenarioState
+	private AdStudent student;
 
-    @ExpectedScenarioState
-    private AdProgram program;
+	@ExpectedScenarioState
+	private AdProgram program;
 
-    @ExpectedScenarioState
-    private AdFaculty faculty;
+	@ExpectedScenarioState
+	private AdFaculty faculty;
 
-    @ExpectedScenarioState
-    private String code;
+	@ExpectedScenarioState
+	private String code;
 
-    private boolean exists;
+	private boolean exists;
 
-    @Pending
-    public ThenICanManageScheduleByProgram I_can_manage_schedule_by_program() {
+	@Pending
+	public ThenICanManageScheduleByProgram I_can_manage_schedule_by_program() {
 
-        return self();
-    }
+		return self();
+	}
 
 }
