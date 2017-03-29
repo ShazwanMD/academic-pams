9222ea87315df039af7937e6bdd14da63c55e3cc
Zaida Nawi <zaida.n@umk.edu.my>
2017-03-29 09:41:55 +0800

    Update When US_AD_PNR_1006

--- back/src/test/java/my/edu/umk/pams/academic/planner/stage/WhenIWantToSetUpCurriculumForAFaculty.java
+++ back/src/test/java/my/edu/umk/pams/academic/planner/stage/WhenIWantToSetUpCurriculumForAFaculty.java
@@ -2,6 +2,8 @@ package my.edu.umk.pams.academic.planner.stage;
 
 import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.annotation.ProvidedScenarioState;
+import com.tngtech.jgiven.integration.spring.JGivenStage;
+
 import my.edu.umk.pams.academic.common.service.CommonService;
 import my.edu.umk.pams.academic.planner.model.*;
 import my.edu.umk.pams.academic.planner.service.PlannerService;
@@ -16,33 +18,45 @@ import org.springframework.beans.factory.annotation.Autowired;
  * @author zaida
  */
 
+@JGivenStage
 public class WhenIWantToSetUpCurriculumForAFaculty extends Stage<WhenIWantToSetUpCurriculumForAFaculty> {
 
-    private static final Logger LOG = LoggerFactory.getLogger(WhenIWantToSetUpCurriculumForAFaculty.class);
+	private static final Logger LOG = LoggerFactory.getLogger(WhenIWantToSetUpCurriculumForAFaculty.class);
 
-    @Autowired
-    private PlannerService plannerService;
+	@Autowired
+	private PlannerService plannerService;
 
-    @Autowired
-    private CommonService commonService;
+	@Autowired
+	private CommonService commonService;
 
-    @ProvidedScenarioState
-    private AdFaculty faculty;
+	@ProvidedScenarioState
+	private AdFaculty faculty;
 
-    public WhenIWantToSetUpCurriculumForAFaculty I_want_to_set_up_curriculum_for_a_faculty_$(String code) {
+	public WhenIWantToSetUpCurriculumForAFaculty I_want_to_set_up_curriculum_for_a_faculty_$(String code) {
 
-        faculty = plannerService.findFacultyByCode(code);
-        AdCurriculum Curriculum = new AdCurriculumImpl();
+		faculty = plannerService.findFacultyByCode("FKP");
 
-        Curriculum.setCode("");
-        ((AdCourse) Curriculum).setFaculty(faculty);
-        ((AdCourse) Curriculum).setClassification(AdAcademicClassification.LEVEL_000);
-        Curriculum.getOrdinal();
-        Curriculum.getPeriod();
-        Curriculum.getProgram();
-        plannerService.saveCurriculum(Curriculum);
-        return self();
+		AdCurriculum Curriculum = new AdCurriculumImpl();
 
-    }
+		AdProgram program = plannerService.findProgramByCode("FKP/MASTER/0001/CRLM/0001");
 
+		Curriculum.setCode(code);
+		Curriculum.setOrdinal(1);
+		Curriculum.setCoreCredit(3);
+		Curriculum.setElectiveCredit(3);
+		Curriculum.setGeneralCredit(3);
+		Curriculum.setRequiredCredit(3);
+		Curriculum.setLanguageCredit(3);
+		Curriculum.setCurriculumCredit(1);
+		Curriculum.setOthersCredit(1);
+		Curriculum.setTotalCredit(3);
+		Curriculum.setMaxPeriod(5);
+		Curriculum.setPeriod(4);
+		Curriculum.setProgram(program);
+
+		plannerService.saveCurriculum(Curriculum);
+
+		return self();
+
+	}
 }
