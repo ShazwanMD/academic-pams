c73c8f90f9b91be920d9d078ad654586f3d49a5a
Abiq Saidi <abiqsaidi@gmail.com>
2017-04-02 16:11:38 +0800

    update method student status

--- back/src/test/java/my/edu/umk/pams/academic/profile/stage/ThenStudentGraduationStatusReviewed.java
+++ back/src/test/java/my/edu/umk/pams/academic/profile/stage/ThenStudentGraduationStatusReviewed.java
@@ -1,22 +1,35 @@
 package my.edu.umk.pams.academic.profile.stage;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 
 import com.tngtech.jgiven.Stage;
-import com.tngtech.jgiven.annotation.Pending;
+import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 import com.tngtech.jgiven.integration.spring.JGivenStage;
+
+import io.jsonwebtoken.lang.Assert;
+import my.edu.umk.pams.academic.identity.model.AdStudent;
+import my.edu.umk.pams.academic.identity.model.AdStudentStatus;
 
 
 
 @JGivenStage
 public class ThenStudentGraduationStatusReviewed extends Stage<ThenStudentGraduationStatusReviewed> {
 
-	private static final Logger LOG = LoggerFactory.getLogger(ThenStudentGraduationStatusReviewed.class);
-	
-	
-	@Pending
-	public ThenStudentGraduationStatusReviewed Student_Graduation_Status_Reviewed() {
+    @ExpectedScenarioState
+    private AdStudent student;
+    
+	@ExpectedScenarioState
+	private AdStudentStatus studentStatus;
+
+	public ThenStudentGraduationStatusReviewed student_graduation_status_reviewed() {
+		
+		student.getIdentityNo();
+    	
+    	AdStudentStatus expectedStatus = student.getStudentStatus();
+    	AdStudentStatus actualStatus = student.getStudentStatus();
+    	
+    	Assert.isTrue(expectedStatus.equals(actualStatus), "exists");
+		
 		
 		return self();
 		
--- back/src/test/java/my/edu/umk/pams/academic/profile/stage/WhenBursaryReviewStudentGraduationStatus.java
+++ back/src/test/java/my/edu/umk/pams/academic/profile/stage/WhenBursaryReviewStudentGraduationStatus.java
@@ -1,23 +1,42 @@
 package my.edu.umk.pams.academic.profile.stage;
 
 import org.slf4j.Logger;
+
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.tngtech.jgiven.Stage;
-import com.tngtech.jgiven.annotation.Pending;
+import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 import com.tngtech.jgiven.integration.spring.JGivenStage;
+
+import my.edu.umk.pams.academic.identity.model.AdStudent;
+import my.edu.umk.pams.academic.identity.model.AdStudentStatus;
+import my.edu.umk.pams.academic.identity.service.IdentityService;
 
 @JGivenStage
 public class WhenBursaryReviewStudentGraduationStatus extends Stage<WhenBursaryReviewStudentGraduationStatus> {
 
 	private static final Logger LOG = LoggerFactory.getLogger(WhenBursaryReviewStudentGraduationStatus.class);
 	
+	@Autowired
+	private IdentityService identityService;
+
+	@ExpectedScenarioState
+	private AdStudent student;
+	
+	@ExpectedScenarioState
+	private AdStudentStatus studentStatus;
 	
 	
+	public WhenBursaryReviewStudentGraduationStatus bursary_review_student_graduation_status(String identityNo) {
 	
-	@Pending
-	public WhenBursaryReviewStudentGraduationStatus Bursary_Review_Student_Graduation_Status() {
-	
+		student = identityService.findStudentByStudentNo(identityNo);
+		AdStudentStatus studentStatus = student.getStudentStatus();
+		
+		LOG.debug("Student's name: {}", student.getName());
+		LOG.debug("Student's status: {}", studentStatus.name());
+		LOG.debug("Student's status: {}", studentStatus.ordinal());
+		
 		return self();
 	
 	}
