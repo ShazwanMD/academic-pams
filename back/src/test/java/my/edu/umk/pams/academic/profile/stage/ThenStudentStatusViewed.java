f000ce9dd14cae6122818eb713f19fa741fd5ec7
Abiq Saidi <abiqsaidi@gmail.com>
2017-04-03 11:15:08 +0800

    Update US_AD_PFL_4002

--- back/src/test/java/my/edu/umk/pams/academic/profile/stage/ThenStudentStatusViewed.java
+++ back/src/test/java/my/edu/umk/pams/academic/profile/stage/ThenStudentStatusViewed.java
@@ -1,13 +1,26 @@
 package my.edu.umk.pams.academic.profile.stage;
 
-import com.tngtech.jgiven.annotation.Pending;
+import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 
-public class ThenStudentStatusViewed {
+import io.jsonwebtoken.lang.Assert;
+import my.edu.umk.pams.academic.identity.model.AdStudent;
+import my.edu.umk.pams.academic.identity.model.AdStudentStatus;
+
+public class ThenStudentStatusViewed extends Stage<ThenStudentStatusViewed> {
 	
-	@Pending
-	public void I_know_student_status_current() {
-		// TODO action to Ihsan
+	   @ExpectedScenarioState
+	    private AdStudent student;
 		
+	public ThenStudentStatusViewed I_know_student_status_current() {
+		
+    	student.getIdentityNo();
+    	
+    	AdStudentStatus expectedStatus = student.getStudentStatus();
+    	AdStudentStatus actualStatus = student.getStudentStatus();
+    	
+    	Assert.isTrue(expectedStatus.equals(actualStatus), "exists");
+		return self();
 	}
 
 }
