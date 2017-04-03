a546821c6bc4ded118c4b1e5ae2b1bb8b2165411
Zaida Nawi <zaida.n@umk.edu.my>
2017-04-03 09:48:18 +0800

    complete refactor US_1006

---
+++ back/src/test/java/my/edu/umk/pams/academic/planner/stage/WhenAdminAddCurriculum.java
@@ -0,0 +1,58 @@
+package my.edu.umk.pams.academic.planner.stage;
+
+import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.ExpectedScenarioState;
+import com.tngtech.jgiven.annotation.ProvidedScenarioState;
+import com.tngtech.jgiven.integration.spring.JGivenStage;
+
+import my.edu.umk.pams.academic.common.service.CommonService;
+import my.edu.umk.pams.academic.planner.model.*;
+import my.edu.umk.pams.academic.planner.service.PlannerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/*
+ * As an admin academic, 
+ * i want to set up curriculum for a faculty, 
+ * so that i can manage schedule by program.
+ * @author zaida
+ */
+
+@JGivenStage
+public class WhenAdminAddCurriculum extends Stage<WhenAdminAddCurriculum> {
+
+	private static final Logger LOG = LoggerFactory.getLogger(WhenAdminAddCurriculum.class);
+
+	@Autowired
+	private PlannerService plannerService;
+
+	@Autowired
+	private CommonService commonService;
+
+	@ExpectedScenarioState
+	private AdProgram program;
+
+	public WhenAdminAddCurriculum Admin_add_curriculu() {
+		// setting up curriculum
+		AdCurriculum curriculum = new AdCurriculumImpl();
+		curriculum.setCode(program.getCode() + "/CRLM/0002");
+		curriculum.setOrdinal(1);
+		curriculum.setCoreCredit(3);
+		curriculum.setElectiveCredit(3);
+		curriculum.setGeneralCredit(3);
+		curriculum.setRequiredCredit(3);
+		curriculum.setLanguageCredit(3);
+		curriculum.setCurriculumCredit(1);
+		curriculum.setOthersCredit(1);
+		curriculum.setTotalCredit(3);
+		curriculum.setMaxPeriod(5);
+		curriculum.setPeriod(4);
+		curriculum.setProgram(program);
+		plannerService.saveCurriculum(curriculum);
+
+		return self();
+
+	}
+
+}
