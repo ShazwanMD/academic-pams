bba87f1c2a39b24408061c583dca00c7863a1563
asyikinresli <asyikinresli@10.3.32.62>
2017-04-04 17:01:55 +0800

    refactor file

--- back/src/test/java/my/edu/umk/pams/academic/term/US_AD_TRM_1004.java
+++ back/src/test/java/my/edu/umk/pams/academic/term/US_AD_TRM_1004.java
@@ -1,8 +1,5 @@
 package my.edu.umk.pams.academic.term;
 
-/**
- * @author asyikin.mr
-*/
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -11,27 +8,34 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
+
 import com.tngtech.jgiven.integration.spring.SpringScenarioTest;
+
 import my.edu.umk.pams.academic.config.TestAppConfiguration;
-import my.edu.umk.pams.academic.term.stage.ThenTheSectionsHaveAppointedStaff;
-import my.edu.umk.pams.academic.term.stage.WhenIAppointStaffToSections;
+import my.edu.umk.pams.academic.term.stage.ThenTheStatusOfAppointmentIsUpdated;
+import my.edu.umk.pams.academic.term.stage.WhenIAppointStaffIntoSection;
 import my.edu.umk.pams.bdd.stage.GivenIAmCPSAdministrator;
 
+/**
+ * @author asyikin.mr
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
 @Transactional
 @ContextConfiguration(classes = TestAppConfiguration.class)
+public class US_AD_TRM_1006 extends
+		SpringScenarioTest<GivenIAmCPSAdministrator, WhenIAppointStaffIntoSection, ThenTheStatusOfAppointmentIsUpdated> {
+	private static final Logger LOG = LoggerFactory.getLogger(US_AD_TRM_1006.class);
 
-public class US_AD_TRM_1004 extends
-		SpringScenarioTest<GivenIAmCPSAdministrator, WhenIAppointStaffToSections, ThenTheSectionsHaveAppointedStaff> {
-
-	private static final Logger LOG = LoggerFactory.getLogger(US_AD_TRM_1004.class);
-	public static final String PROGRAM_CODE = "FKP/MASTER/0001";
+	// from data/AD_ACTR.sql
+	public static final String STAFF_NO = "01001A";
+	// from data/AD_OFRG.sql
+	public static final String SECTION_CANONICAL_CODE = "FKP/PHD/0001/DDA2113/201720181";
 
 	@Test
-	@Rollback(true)
+	@Rollback
 	public void scenario1() {
-		given().I_am_a_CPS_administrator_in_current_academic_session().and().I_pick_program_$(PROGRAM_CODE);
-		when().I_appoint_staff_for_sections();
-		then().the_sections_have_appointed_staff();
+		given().I_am_a_CPS_administrator_in_current_academic_session().and().I_pick_section_$(SECTION_CANONICAL_CODE);
+		when().I_appoint_a_staff_$_into_section(STAFF_NO);
+		then().the_number_of_appointed_staff_increased();
 	}
 }
--- back/src/test/java/my/edu/umk/pams/academic/term/US_AD_TRM_1006.java
+++ back/src/test/java/my/edu/umk/pams/academic/term/US_AD_TRM_1006.java
@@ -1,41 +1,33 @@
 package my.edu.umk.pams.academic.term;
 
+import com.tngtech.jgiven.integration.spring.SpringScenarioTest;
+import my.edu.umk.pams.academic.config.TestAppConfiguration;
+import my.edu.umk.pams.academic.term.stage.ThenTheOfferedCourseHasAMaxAllowQuota;
+import my.edu.umk.pams.academic.term.stage.WhenIWantToSetCapacityForOfferedCourse;
+import my.edu.umk.pams.bdd.stage.GivenIAmCPSAdministrator;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.tngtech.jgiven.integration.spring.SpringScenarioTest;
-
-import my.edu.umk.pams.academic.config.TestAppConfiguration;
-import my.edu.umk.pams.academic.term.stage.ThenTheStatusOfAppointmentIsUpdated;
-import my.edu.umk.pams.academic.term.stage.WhenIAppointStaffIntoSection;
-import my.edu.umk.pams.bdd.stage.GivenIAmCPSAdministrator;
-
 /**
- * @author asyikin.mr
+ * @author asyikin.mr@umk and ziana
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @Transactional
 @ContextConfiguration(classes = TestAppConfiguration.class)
-public class US_AD_TRM_1006 extends
-		SpringScenarioTest<GivenIAmCPSAdministrator, WhenIAppointStaffIntoSection, ThenTheStatusOfAppointmentIsUpdated> {
-	private static final Logger LOG = LoggerFactory.getLogger(US_AD_TRM_1006.class);
+public class US_AD_TRM_0007 extends
+		SpringScenarioTest<GivenIAmCPSAdministrator, WhenIWantToSetCapacityForOfferedCourse, ThenTheOfferedCourseHasAMaxAllowQuota> {
 
-	// from data/AD_ACTR.sql
-	public static final String STAFF_NO = "01001A";
-	// from data/AD_OFRG.sql
-	public static final String SECTION_CANONICAL_CODE = "FKP/PHD/0001/DDA2113/201720181";
+	public static final String FACULTY_CODE = "FKP";
 
 	@Test
 	@Rollback
 	public void scenario1() {
-		given().I_am_a_CPS_administrator_in_current_academic_session().and().I_pick_section_$(SECTION_CANONICAL_CODE);
-		when().I_appoint_a_staff_$_into_section(STAFF_NO);
-		then().the_number_of_appointed_staff_increased();
+		given().I_am_a_CPS_administrator_in_current_academic_session().and().I_pick_faculty_$(FACULTY_CODE);
+		when().I_create_and_set_offering_capacity();
+		then().the_offered_course_has_a_maximum_quota();
 	}
 }
