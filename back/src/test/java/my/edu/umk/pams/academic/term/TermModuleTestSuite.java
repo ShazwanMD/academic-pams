2ee86de0daf833e79bd18446dfbcca2a04972205
asyikinresli <asyikinresli@10.3.32.62>
2017-04-04 17:06:34 +0800

    update file

--- back/src/test/java/my/edu/umk/pams/academic/term/TermModuleTestSuite.java
+++ back/src/test/java/my/edu/umk/pams/academic/term/TermModuleTestSuite.java
@@ -11,14 +11,21 @@ import org.junit.runners.Suite;
 @Suite.SuiteClasses({
         US_AD_TRM_0001.class,
         US_AD_TRM_0002.class,
-        US_AD_TRM_0007.class,
         US_AD_TRM_0009.class,
-        US_AD_TRM_10001.class,
         US_AD_TRM_1001.class,
         US_AD_TRM_1002.class,
         US_AD_TRM_1003.class,
+        US_AD_TRM_1004.class,
         US_AD_TRM_1005.class,
         US_AD_TRM_1006.class,
+        US_AD_TRM_1008.class,
+        US_AD_TRM_1009.class,
+        US_AD_TRM_1011.class,
+        US_AD_TRM_1014.class,
+        US_AD_TRM_1015.class,
+        US_AD_TRM_1017.class,
+        US_AD_TRM_1019.class,
+        US_AD_TRM_1020.class,
        
 })
 public class TermModuleTestSuite {
