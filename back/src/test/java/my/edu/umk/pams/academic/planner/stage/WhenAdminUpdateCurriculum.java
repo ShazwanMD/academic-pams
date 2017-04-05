0932426d4c5d002df8126cb83ca218df4522479a
Zaida Nawi <zaida.n@umk.edu.my>
2017-04-05 14:14:26 +0800

    update When US_AD_1014

--- back/src/test/java/my/edu/umk/pams/academic/planner/stage/WhenAdminUpdateCurriculum.java
+++ back/src/test/java/my/edu/umk/pams/academic/planner/stage/WhenAdminUpdateCurriculum.java
@@ -1,17 +1,27 @@
 package my.edu.umk.pams.academic.planner.stage;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 import com.tngtech.jgiven.annotation.ProvidedScenarioState;
+import com.tngtech.jgiven.integration.spring.JGivenStage;
 
 import my.edu.umk.pams.academic.common.service.CommonService;
 
 import my.edu.umk.pams.academic.planner.model.AdCurriculum;
+import my.edu.umk.pams.academic.planner.model.AdCurriculumImpl;
+import my.edu.umk.pams.academic.planner.model.AdFaculty;
 import my.edu.umk.pams.academic.planner.model.AdProgram;
+import my.edu.umk.pams.academic.planner.model.AdProgramLevel;
 import my.edu.umk.pams.academic.planner.service.PlannerService;
 
+@JGivenStage
 public class WhenAdminUpdateCurriculum extends Stage<WhenAdminUpdateCurriculum> {
+
+	private static final Logger LOG = LoggerFactory.getLogger(WhenAdminUpdateCurriculum.class);
 
 	@Autowired
 	private PlannerService plannerService;
@@ -19,13 +29,35 @@ public class WhenAdminUpdateCurriculum extends Stage<WhenAdminUpdateCurriculum> 
 	@Autowired
 	private CommonService commonService;
 
-	@ProvidedScenarioState
+	@ExpectedScenarioState
 	private AdProgram program;
 
-	@ProvidedScenarioState
+	@ExpectedScenarioState
 	private AdCurriculum curriculum;
 
 	public WhenAdminUpdateCurriculum Admin_update_curriculum_$(String Code) {
+
+		curriculum = plannerService.findCurriculumByCode("FIAT/PHD/0001/CRLM/0001");
+
+		
+		// setting up curriculum
+		curriculum.setCode("FIAT/PHD/0001/CRLM/CRLM/0003");
+		curriculum.setOrdinal(1);
+		curriculum.setCoreCredit(2);
+		curriculum.setElectiveCredit(3);
+		curriculum.setGeneralCredit(3);
+		curriculum.setRequiredCredit(3);
+		curriculum.setLanguageCredit(3);
+		curriculum.setCurriculumCredit(1);
+		curriculum.setOthersCredit(1);
+		curriculum.setTotalCredit(3);
+		curriculum.setMaxPeriod(5);
+		curriculum.setPeriod(4);
+		curriculum.setProgram(program);
+
+		plannerService.updateCurriculum(curriculum);
+
+		LOG.debug("curriculum : {}", curriculum.getCode());
 
 		return self();
 	}
