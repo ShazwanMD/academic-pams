abced0c99d653de440e8f2c572736ff4cbab5f47
zaida <zaida@10.3.32.165>
2017-04-09 11:53:00 +0800

    Creat file US_AD_TRM_1013

---
+++ back/src/test/java/my/edu/umk/pams/academic/term/stage/ThenTheAdmissionStatusIsUpdated.java
@@ -0,0 +1,19 @@
+package my.edu.umk.pams.academic.term.stage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.integration.spring.JGivenStage;
+
+@JGivenStage
+public class ThenTheAdmissionStatusIsUpdated extends Stage<ThenTheAdmissionStatusIsUpdated> {
+	private static final Logger LOG = LoggerFactory.getLogger(ThenTheAdmissionStatusIsUpdated.class);
+
+	public void the_admission_status_is_updated() {
+		
+		
+	}
+	
+
+}
