9907a9616332b4fe4b13b9dadebf00a80daa07db
asyikinresli <asyikinresli@10.3.32.62>
2017-04-10 15:55:56 +0800

    create new file US_AD_TRM_1010

---
+++ back/src/test/java/my/edu/umk/pams/academic/term/US_AD_TRM_1010.java
@@ -0,0 +1,33 @@
+package my.edu.umk.pams.academic.term;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+import com.tngtech.jgiven.annotation.As;
+import com.tngtech.jgiven.integration.spring.SpringScenarioTest;
+import my.edu.umk.pams.academic.config.TestAppConfiguration;
+import my.edu.umk.pams.academic.term.stage.ThenTheAdvisorIsUpdated;
+import my.edu.umk.pams.academic.term.stage.WhenChangeAdvisor;
+import my.edu.umk.pams.bdd.stage.GivenIAmCPSAdministrator;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+@ContextConfiguration(classes = TestAppConfiguration.class)
+@As("As a academic administrator,I want to change advisor for student so that the advisor is updated")
+
+public class US_AD_TRM_1010 extends SpringScenarioTest<GivenIAmCPSAdministrator, WhenChangeAdvisor, ThenTheAdvisorIsUpdated> {
+	private static final Logger LOG = LoggerFactory.getLogger(US_AD_TRM_1010.class);
+	
+	@Test
+	@Rollback(true)
+	public void scenario01() {
+		given().I_am_a_CPS_administrator_in_current_academic_session();
+		when().I_want_to_change_advisor();
+		then().The_advisor_is_updated();
+	}
+}
