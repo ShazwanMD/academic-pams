6f990be18cb9b12cd2eb4bb58f3580ec8295a118
zaida <zaida@10.3.32.174>
2017-04-10 12:29:41 +0800

    Create new file US_AD_TRM_1008

---
+++ back/src/test/java/my/edu/umk/pams/academic/term/US_AD_TRM_1008.java
@@ -0,0 +1,25 @@
+package my.edu.umk.pams.academic.term;
+
+import org.junit.Test;
+import org.springframework.test.annotation.Rollback;
+
+import com.tngtech.jgiven.integration.spring.SpringScenarioTest;
+
+import my.edu.umk.pams.academic.term.stage.ThenTheEnrollmentIsUpdated;
+import my.edu.umk.pams.academic.term.stage.ThenTheEnrollmentStatusIsUpdated;
+import my.edu.umk.pams.academic.term.stage.WhenITransferSection;
+import my.edu.umk.pams.academic.term.stage.WhenIUpdateEnrollmentStatus;
+import my.edu.umk.pams.bdd.stage.GivenIAmCPSAdministrator;
+
+public class US_AD_TRM_1008 extends
+SpringScenarioTest<GivenIAmCPSAdministrator, WhenITransferSection, ThenTheEnrollmentIsUpdated> {
+	
+	@Test
+	@Rollback
+	public void scenario1() {
+		given().I_am_a_CPS_administrator();
+		when().I_transfer_section();
+		then().the_enrollment_is_updated();
+	}
+
+}
