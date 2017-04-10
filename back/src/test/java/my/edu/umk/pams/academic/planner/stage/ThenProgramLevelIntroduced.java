f33e2f464b0460945b504c874474b96f614fc56d
Zaida Nawi <zaida.n@umk.edu.my>
2017-04-10 17:16:12 +0800

    Update US_AD_PNR_1008

--- back/src/test/java/my/edu/umk/pams/academic/planner/stage/ThenProgramLevelIntroduced.java
+++ back/src/test/java/my/edu/umk/pams/academic/planner/stage/ThenProgramLevelIntroduced.java
@@ -7,7 +7,11 @@ import io.jsonwebtoken.lang.Assert;
 import my.edu.umk.pams.academic.planner.model.AdFaculty;
 import my.edu.umk.pams.academic.planner.model.AdProgram;
 import my.edu.umk.pams.academic.planner.model.AdProgramLevel;
+import my.edu.umk.pams.academic.planner.model.AdProgramType;
 import my.edu.umk.pams.academic.planner.service.PlannerService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
@@ -15,6 +19,8 @@ import java.util.List;
 @JGivenStage
 public class ThenProgramLevelIntroduced extends Stage<ThenProgramLevelIntroduced> {
 
+	  private static final Logger LOG = LoggerFactory.getLogger(ThenProgramInfomationReviewed.class);
+		
     @Autowired
     private PlannerService plannerService;
 
@@ -25,6 +31,9 @@ public class ThenProgramLevelIntroduced extends Stage<ThenProgramLevelIntroduced
     private AdProgram program;
     
     @ExpectedScenarioState
+    private AdProgramLevel programLevel;
+    
+    @ExpectedScenarioState
     private String PROGRAM_CODE;
     
     @ExpectedScenarioState
@@ -33,8 +42,25 @@ public class ThenProgramLevelIntroduced extends Stage<ThenProgramLevelIntroduced
 
     public ThenProgramLevelIntroduced program_level_$_introduced(String PROGRAM_CODE) {
 
-//        List<AdProgramLevel> ProgramLevels = plannerService.findProgramLevels();
-       // Assert.notEmpty(ProgramLevels);
+    	List< AdProgramLevel> programLevel = plannerService.findProgramLevels();
+		Assert.notEmpty(programLevel, "programLevel should not be empty");
+		
+    	
+    	for ( AdProgramLevel progLevel : programLevel) {
+            LOG.debug("program level code : {}",progLevel.getCode());
+            Assert.notNull(progLevel.getCode(), "programLevel code should not be empty");
+        }
+    	
+    	for ( AdProgramLevel progLevel : programLevel) {
+            LOG.debug("program level Description: {}",progLevel.getDescription());
+            Assert.notNull(progLevel.getDescription(), "programLevel description should not be empty");
+        }
+    	
+    	for ( AdProgramLevel progLevel : programLevel) {
+            LOG.debug("program level Type : {}", progLevel.getType().MASTER);
+            Assert.notNull(progLevel.getType().MASTER, "programLevel type should not be empty");
+        }
+
 
         return self();
     }
