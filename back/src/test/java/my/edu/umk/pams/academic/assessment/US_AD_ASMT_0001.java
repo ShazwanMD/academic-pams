3a08db36ef9c0cd92950412acf5f1654e035b39e
Abiq Saidi <abiqsaidi@gmail.com>
2017-04-11 15:58:51 +0800

    Add US_AD_ASMT_0001 template

---
+++ back/src/test/java/my/edu/umk/pams/academic/assessment/US_AD_ASMT_0001.java
@@ -0,0 +1,5 @@
+package my.edu.umk.pams.academic.assessment;
+
+public class US_AD_ASMT_0001 {
+
+}
---
+++ back/src/test/java/my/edu/umk/pams/academic/assessment/stage/ThenStudentResultReviewed.java
@@ -0,0 +1,5 @@
+package my.edu.umk.pams.academic.assessment.stage;
+
+public class ThenStudentResultReviewed {
+
+}
