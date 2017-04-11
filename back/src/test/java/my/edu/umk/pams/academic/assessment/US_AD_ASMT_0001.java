ad47f4156d1af5e22d1d05b44a5e9384f093f34e
Shazwan MD <shazwan.md@umk.edu.my>
2017-04-11 17:03:03 +0800

    add @AS

--- back/src/test/java/my/edu/umk/pams/academic/assessment/US_AD_ASMT_0001.java
+++
@@ -1,5 +0,0 @@
-package my.edu.umk.pams.academic.assessment;
-
-public class US_AD_ASMT_0001 {
-
-}
--- back/src/test/java/my/edu/umk/pams/academic/assessment/US_AD_ASMT_1002.java
+++ back/src/test/java/my/edu/umk/pams/academic/assessment/US_AD_ASMT_1002.java
@@ -1,5 +1,18 @@
 package my.edu.umk.pams.academic.assessment;
 
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.tngtech.jgiven.annotation.As;
+
+import my.edu.umk.pams.academic.config.TestAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+@ContextConfiguration(classes = TestAppConfiguration.class)
+@As("As a student,I want to view student resultsSo that I can know my results.")
 public class US_AD_ASMT_1002 {
 
 }
