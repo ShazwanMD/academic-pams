2ba40566c18abddbc83b6c88edf75bb0c6215d07
Shazwan MD <shazwan.md@umk.edu.my>
2017-04-12 10:19:31 +0800

    update method

--- back/src/test/java/my/edu/umk/pams/academic/profile/stage/WhenBursaryReviewStudentGraduationStatus.java
+++ back/src/test/java/my/edu/umk/pams/academic/profile/stage/WhenBursaryReviewStudentGraduationStatus.java
@@ -1,5 +1,7 @@
 package my.edu.umk.pams.academic.profile.stage;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 
 import org.slf4j.LoggerFactory;
@@ -12,6 +14,12 @@ import com.tngtech.jgiven.integration.spring.JGivenStage;
 import my.edu.umk.pams.academic.identity.model.AdStudent;
 import my.edu.umk.pams.academic.identity.model.AdStudentStatus;
 import my.edu.umk.pams.academic.identity.service.IdentityService;
+import my.edu.umk.pams.academic.planner.model.AdCohort;
+import my.edu.umk.pams.academic.planner.model.AdCourse;
+import my.edu.umk.pams.academic.planner.model.AdFaculty;
+import my.edu.umk.pams.academic.planner.model.AdProgram;
+import my.edu.umk.pams.academic.planner.model.AdProgramLevel;
+import my.edu.umk.pams.academic.planner.service.PlannerService;
 
 @JGivenStage
 public class WhenBursaryReviewStudentGraduationStatus extends Stage<WhenBursaryReviewStudentGraduationStatus> {
@@ -20,23 +28,45 @@ public class WhenBursaryReviewStudentGraduationStatus extends Stage<WhenBursaryR
 	
 	@Autowired
 	private IdentityService identityService;
-
-	@ExpectedScenarioState
-	private AdStudent student;
 	
-	@ExpectedScenarioState
+	@Autowired
+	private PlannerService plannerService;
+
+    @ExpectedScenarioState
+	private AdStudent student;
+    
+    @ExpectedScenarioState
+   	private AdProgram program;
+    
+    @ExpectedScenarioState
+   	private AdProgramLevel level;
+    
+    @ExpectedScenarioState
+   	private List<AdCourse> courses;
+    
+    @ExpectedScenarioState
+   	private AdFaculty faculty;
+    
+    @ExpectedScenarioState
+   	private AdCohort cohort;
+
+    @ExpectedScenarioState
 	private AdStudentStatus studentStatus;
 	
 	
 	public WhenBursaryReviewStudentGraduationStatus bursary_review_student_graduation_status(String identityNo) {
-	
 		student = identityService.findStudentByMatricNo(identityNo);
 		AdStudentStatus studentStatus = student.getStudentStatus();
 		
+		if(studentStatus == AdStudentStatus.GRADUATED){
 		LOG.debug("Student's name: {}", student.getName());
 		LOG.debug("Student's status: {}", studentStatus.name());
-		LOG.debug("Student's status: {}", studentStatus.ordinal());
+		LOG.debug("Congratulations");
 		
+		}else {
+			
+			LOG.debug("Not Qualify to graduate");
+		}
 		return self();
 	
 	}
