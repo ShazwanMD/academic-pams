b87c02df39adae5e4e29d5e786dafbe7fc5d70f2
ain <ain@10.3.32.66>
2017-04-13 13:42:14 +0800

    add file US_AD_TRM_1027

---
+++ back/src/test/java/my/edu/umk/pams/academic/term/stage/ThenSectionsCurrentAppointedStaff.java
@@ -0,0 +1,11 @@
+package my.edu.umk.pams.academic.term.stage;
+
+public class ThenSectionsCurrentAppointedStaff {
+
+	public void Section_current_appointed_staff_info() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
