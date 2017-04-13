ee1bb02617b844b257958daa20edc66c1b725a9b
Zaida Nawi <zaida.n@umk.edu.my>
2017-04-13 15:01:50 +0800

    UPdate when

--- back/src/test/java/my/edu/umk/pams/academic/term/stage/WhenWantToViewOpenDateOfEnrollment.java
+++ back/src/test/java/my/edu/umk/pams/academic/term/stage/WhenWantToViewOpenDateOfEnrollment.java
@@ -1,4 +1,9 @@
 package my.edu.umk.pams.academic.term.stage;
+/**
+ * @author zaida_nawi
+ **/
+
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,13 +13,20 @@ import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 import com.tngtech.jgiven.annotation.Pending;
 import com.tngtech.jgiven.annotation.ProvidedScenarioState;
 import com.tngtech.jgiven.integration.spring.JGivenStage;
+
+import io.jsonwebtoken.lang.Assert;
+import my.edu.umk.pams.academic.AcademicConstants;
 import my.edu.umk.pams.academic.identity.model.AdStudent;
 import my.edu.umk.pams.academic.planner.model.AdProgram;
 import my.edu.umk.pams.academic.planner.service.PlannerService;
+import my.edu.umk.pams.academic.system.model.AdConfiguration;
+import my.edu.umk.pams.academic.system.service.SystemService;
+import my.edu.umk.pams.academic.term.model.AdEnrollment;
+import my.edu.umk.pams.academic.term.model.AdOffering;
 import my.edu.umk.pams.academic.term.model.AdSection;
 import my.edu.umk.pams.academic.term.service.TermService;
 
-@Pending
+
 @JGivenStage
 public class WhenWantToViewOpenDateOfEnrollment extends Stage<WhenWantToViewOpenDateOfEnrollment> {
 
@@ -25,6 +37,9 @@ public class WhenWantToViewOpenDateOfEnrollment extends Stage<WhenWantToViewOpen
 
 	@Autowired
 	private PlannerService plannerService;
+	
+	@Autowired
+	private SystemService systemService;
 
 	@ExpectedScenarioState
 	private AdProgram program;
@@ -34,11 +49,44 @@ public class WhenWantToViewOpenDateOfEnrollment extends Stage<WhenWantToViewOpen
 
 	@ProvidedScenarioState
 	private AdSection section;
+	
+	@ProvidedScenarioState
+	private AdConfiguration configuration;
 
 	public WhenWantToViewOpenDateOfEnrollment Want_to_view_open_date_of_enrollment() {
 
-		// process to view open date in ad_cnfg table
+	//	section = termService.findSectionByCanonicalCode("A01/PHD/0001/DDA2113/201720181");
+	//	Assert.notNull(section, "section should be not null");
+	//	AdOffering offering = termService.findOfferingByCanonicalCode("A01/PHD/0001/DDA2113");
 		
+		
+	//	section = termService.findSectionByCanonicalCode("A01/PHD/0001/DDA2113/201720181");
+	//	Assert.notNull(section, "section should be not null");
+
+	//		LOG.debug("section id:{}", section.getId());
+	//		LOG.debug("capacity section: {}", section.getCapacity());
+	//		LOG.debug("ordinal section: {}", section.getOrdinal());
+
+			
+			configuration = systemService.findConfigurationByKey(AcademicConstants.ENROLLMENT_STARTDATE_OPEN);
+			configuration.setValue("30/04/2017");
+			systemService.updateConfiguration(configuration);
+			LOG.debug("description : {}", configuration.getDescription());
+			LOG.debug("Open Enrollment Date : {}", configuration.getValue());
+			
+		
+
+		
+		
+	
+
+		
+		//List<InIntakeApplication> applications = applicationService.findIntakeApplications(intake,
+				//InBidStatus.SUBMITTED);
+		//AdConfiguration configuration =  = systemService.findConfigurationByKey(key)
+	//	LOG.debug("value: {}", configuration.getValue());
+
+		// process to view open date in ad_cnfg table
 
 		return self();
 	}
