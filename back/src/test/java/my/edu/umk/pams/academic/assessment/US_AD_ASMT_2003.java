17d24165b81d6788e0bf4acaddb1c85c515b8014
Shazwan MD <shazwan.md@umk.edu.my>
2017-04-16 10:34:18 +0800

    create method for view exam info

--- back/src/test/java/my/edu/umk/pams/academic/assessment/US_AD_ASMT_2003.java
+++ back/src/test/java/my/edu/umk/pams/academic/assessment/US_AD_ASMT_2003.java
@@ -10,6 +10,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.tngtech.jgiven.annotation.As;
+import com.tngtech.jgiven.annotation.ProvidedScenarioState;
 import com.tngtech.jgiven.integration.spring.SpringScenarioTest;
 
 import my.edu.umk.pams.academic.assessment.stage.ThenExamVivaInfoReviewed;
@@ -21,25 +22,28 @@ import my.edu.umk.pams.bdd.stage.GivenIAmAcademicStaff;
 @Transactional
 @ContextConfiguration(classes = TestAppConfiguration.class)
 @As("As a supervisor,I want to view exam/viva info So that I can view viva/exam information.")
-public class US_AD_ASMT_2003 extends SpringScenarioTest<GivenIAmAcademicStaff, WhenIReviewExamVivaInfo, ThenExamVivaInfoReviewed> {
+public class US_AD_ASMT_2003
+		extends SpringScenarioTest<GivenIAmAcademicStaff, WhenIReviewExamVivaInfo, ThenExamVivaInfoReviewed> {
 
-	 private static final Logger LOG = LoggerFactory.getLogger(US_AD_ASMT_2003.class);
-	 
+	private static final Logger LOG = LoggerFactory.getLogger(US_AD_ASMT_2003.class);
 
-	    @Test
-	    @Rollback
-	    public void scenarioEXAM() {
-	        given().i_am_a_staff_in_current_academic_session();
-	        when().i_review_examination_info();
-	        then().exam_info_reviewed();
-	    }
-	    
-	    @Test
-	    @Rollback
-	    public void scenarioVIVA() {
-	        given().i_am_a_staff_in_current_academic_session();
-	        when().i_review_viva_info();
-	        then().viva_info_reviewed();
-	    }
-	 
+	@ProvidedScenarioState
+	private String facultyCode = "A10";
+
+	@Test
+	@Rollback
+	public void scenarioEXAM() {
+		given().i_am_a_staff_in_current_academic_session();
+		when().i_review_examination_info();
+		then().exam_info_reviewed();
+	}
+
+	@Test
+	@Rollback
+	public void scenarioVIVA() {
+		given().i_am_a_staff_in_current_academic_session();
+		when().i_review_viva_info();
+		then().viva_info_reviewed();
+	}
+
 }
