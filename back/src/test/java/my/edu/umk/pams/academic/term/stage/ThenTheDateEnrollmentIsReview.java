038f88c904ace9742d1f1658511a166c4c7e2dd7
Zaida Nawi <zaida.n@umk.edu.my>
2017-04-16 17:15:14 +0800

    update Then

--- back/src/test/java/my/edu/umk/pams/academic/term/stage/ThenTheDateEnrollmentIsReview.java
+++ back/src/test/java/my/edu/umk/pams/academic/term/stage/ThenTheDateEnrollmentIsReview.java
@@ -12,15 +12,25 @@ import com.tngtech.jgiven.annotation.Pending;
 import com.tngtech.jgiven.integration.spring.JGivenStage;
 import io.jsonwebtoken.lang.Assert;
 import my.edu.umk.pams.academic.planner.model.AdAcademicSession;
+import my.edu.umk.pams.academic.planner.model.AdCourse;
+import my.edu.umk.pams.academic.planner.service.PlannerService;
 import my.edu.umk.pams.academic.system.model.AdConfiguration;
 import my.edu.umk.pams.academic.system.service.SystemService;
+import my.edu.umk.pams.academic.term.model.AdOffering;
 import my.edu.umk.pams.academic.term.model.AdSection;
+import my.edu.umk.pams.academic.term.service.TermService;
 
 
 @JGivenStage
 public class ThenTheDateEnrollmentIsReview extends Stage<ThenTheDateEnrollmentIsReview> {
 	private static final Logger LOG = LoggerFactory.getLogger(ThenTheDateEnrollmentIsReview.class);
 
+	@Autowired
+	private TermService termService;
+	
+	@Autowired
+	private PlannerService plannerService;
+	
 	@ExpectedScenarioState
 	private AdAcademicSession academicSession;
 
@@ -31,10 +41,26 @@ public class ThenTheDateEnrollmentIsReview extends Stage<ThenTheDateEnrollmentIs
 	private AdConfiguration configuration;
 
 	public ThenTheDateEnrollmentIsReview Know_the_enrollment_interval_period() {
-
 		
-		//TODO COUNT 
-		Assert.notNull(configuration, "section should be not null");
+		
+		AdOffering offering = termService.findOfferingByCanonicalCode("A01/PHD/0001/DDA2113");
+	    termService.countEnrollment(academicSession, offering);
+	    LOG.debug("number of available course :{}", termService.countEnrollment(academicSession, offering));
+		
+	   // plannerService.countAvailableCourse(academicSession);
+	   // LOG.debug("number of available course {} :", plannerService.countAvailableCourse(academicSession) );
+	    //contoh count jum. course dlm academic session
+	    
+	    
+	    
+	    
+//	    Integer countCourse(String filter);
+//	    Integer countCourse(AdFaculty faculty);
+//	    Integer countCourse(String filter, AdFaculty faculty);
+//	    Integer countCourse(boolean authorized, String filter);
+//	    Integer countAvailableCourse(AdAcademicSession academicSession);
+		
+	//	Assert.notNull(configuration, "section should be not null");
 		
 		
 		return self();
