d76dace3fc2b5a9bf41ec7c8bf6ae0bf76000d87
asyikinresli <asyikinresli@10.3.32.62>
2017-04-16 16:00:34 +0800

    update file method

--- back/src/test/java/my/edu/umk/pams/academic/term/stage/WhenIAddEnrollmentCourses.java
+++ back/src/test/java/my/edu/umk/pams/academic/term/stage/WhenIAddEnrollmentCourses.java
@@ -2,7 +2,6 @@ package my.edu.umk.pams.academic.term.stage;
 
 //@author:asyikin
 import org.slf4j.Logger;
-
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.tngtech.jgiven.Stage;
