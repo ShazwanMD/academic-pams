e62aee98a401222da367a867383b20f538c3949f
Zaida Nawi <zaida.n@umk.edu.my>
2017-04-16 10:31:53 +0800

    add @issue

--- back/src/test/java/my/edu/umk/pams/academic/planner/US_AD_PNR_1002.java
+++ back/src/test/java/my/edu/umk/pams/academic/planner/US_AD_PNR_1002.java
@@ -8,7 +8,6 @@ import my.edu.umk.pams.academic.planner.stage.WhenAdminAddCourse;
 import my.edu.umk.pams.bdd.stage.GivenIAmCPSAdministrator;
 import my.edu.umk.pams.bdd.tags.Issue;
 import my.edu.umk.pams.bdd.tags.Submodule;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -18,25 +17,28 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * @author zaida_nawi
+ **/
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @Transactional
 @ContextConfiguration(classes = TestAppConfiguration.class)
 @As("As an academic administrator, i want to set up courses for a faculty so that a faculty will be populated with courses")
-@Issue("PAMA-18")
+@Issue("PAMA-19")
 @Submodule("Planner")
 
-public class US_AD_PNR_1002 extends SpringScenarioTest<GivenIAmCPSAdministrator, WhenAdminAddCourse, ThenFacultyHasCourse> {
+public class US_AD_PNR_1002
+		extends SpringScenarioTest<GivenIAmCPSAdministrator, WhenAdminAddCourse, ThenFacultyHasCourse> {
 
-    private static final Logger LOG = LoggerFactory.getLogger(US_AD_PNR_1002.class);
-    public static final String FACULTY_CODE = "A01";
+	private static final Logger LOG = LoggerFactory.getLogger(US_AD_PNR_1002.class);
+	public static final String FACULTY_CODE = "A01";
 
-    @Test
-    @Rollback
-    public void scenario1() {
-        given().I_am_a_CPS_administrator();
-        when().Admin_add_a_course_to_faculty_code_$(FACULTY_CODE);
-        then().faculty_has_courses();
-    }
+	@Test
+	@Rollback
+	public void scenario1() {
+		given().I_am_a_CPS_administrator();
+		when().Admin_add_a_course_to_faculty_code_$(FACULTY_CODE);
+		then().faculty_has_courses();
+	}
 }
-
