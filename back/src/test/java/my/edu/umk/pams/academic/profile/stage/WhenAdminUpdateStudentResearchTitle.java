652a142509ce96fd0696ea68e2ff5354c1a80af9
Shazwan MD <shazwan.md@umk.edu.my>
2017-04-17 09:02:22 +0800

    fix error

--- back/src/test/java/my/edu/umk/pams/academic/profile/stage/WhenAdminUpdateStudentResearchTitle.java
+++ back/src/test/java/my/edu/umk/pams/academic/profile/stage/WhenAdminUpdateStudentResearchTitle.java
@@ -3,10 +3,12 @@ package my.edu.umk.pams.academic.profile.stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
 
 import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 import com.tngtech.jgiven.annotation.Pending;
+import com.tngtech.jgiven.annotation.ProvidedScenarioState;
 import com.tngtech.jgiven.integration.spring.JGivenStage;
 
 import my.edu.umk.pams.academic.identity.model.AdStudent;
@@ -15,6 +17,8 @@ import my.edu.umk.pams.academic.planner.model.AdAcademicClassification;
 import my.edu.umk.pams.academic.planner.model.AdCourse;
 import my.edu.umk.pams.academic.planner.model.AdCourseImpl;
 import my.edu.umk.pams.academic.planner.model.AdFaculty;
+import my.edu.umk.pams.academic.planner.model.AdProgram;
+import my.edu.umk.pams.academic.planner.model.AdProgramLevel;
 import my.edu.umk.pams.academic.planner.service.PlannerService;
 
 @JGivenStage
@@ -26,36 +30,46 @@ public class WhenAdminUpdateStudentResearchTitle extends Stage<WhenAdminUpdateSt
 	private PlannerService plannerService;
 	
 	
-	@ExpectedScenarioState
+	@ProvidedScenarioState
 	private AdStudent student;
 	
-	@ExpectedScenarioState
+	@ProvidedScenarioState
 	private AdCourse course;
 	
-	@ExpectedScenarioState
+	@ProvidedScenarioState
 	private AdFaculty faculty;
+	
+
+	@ProvidedScenarioState
+	private AdProgramLevel level;
 	
 	public WhenAdminUpdateStudentResearchTitle Admin_update_student_research_title() {
 		
 		AdFaculty faculty = plannerService.findFacultyByCode("CPS");
-		AdCourse course = plannerService.findCourseByCode("MAP");
+		Assert.notNull(faculty, "Faculty Must Not NULL");
+		course = plannerService.findCourseByCodeAndFaculty("MAP", faculty);
+		Assert.notNull(course, "Course Must Not NULL");
 		
+				
 		course.setFaculty(faculty);
-		course.setCode("MAP1");
-		course.setCredit(5);
-		course.setClassification(AdAcademicClassification.LEVEL_DOCTORATE);
-		course.setTitle("SAM");
-		course.setTitleEn("SAM");
-		course.setTitleMs("SAMS");
+		course.setCode("FKP/MASTER/MAP1.1");
+		course.setClassification(AdAcademicClassification.LEVEL_MASTER);
+		course.setCredit(45);
+		course.setTitle("Master of Entrepreneurship Mathematical Science");
+		course.setTitleEn("Master of Entrepreneurship Mathematical Science");
+		course.setTitleMs("Master of Entrepreneurship Mathematical Science");
 		
 		plannerService.updateCourse(faculty, course);
 		
-		LOG.debug("Course Detail :{}", course.getCode());
-		LOG.debug("Course Detail :{}", course.getTitle());
-		LOG.debug("Course Detail :{}", course.getTitleEn());
-		LOG.debug("Course Detail :{}", course.getTitleMs());
-		LOG.debug("Course Detail :{}", course.getCredit());
-		LOG.debug("Course Detail :{}", course.getClassification());
+		LOG.debug("course Detail :{}", course.getCode());
+		LOG.debug("course Detail :{}", course.getFaculty());
+		LOG.debug("course Detail :{}", course.getClassification());
+		LOG.debug("course Detail :{}", course.getCredit());
+		LOG.debug("course Detail :{}", course.getTitle());
+		LOG.debug("course Detail :{}", course.getTitleEn());
+		LOG.debug("course Detail :{}", course.getTitleMs());
+		LOG.debug("");
+	
 		
 		return self();
 		
@@ -63,16 +77,18 @@ public class WhenAdminUpdateStudentResearchTitle extends Stage<WhenAdminUpdateSt
 
 	public WhenAdminUpdateStudentResearchTitle Admin_update_student_research_titles() {
 		
-		AdFaculty faculty = plannerService.findFacultyByCode("A10");
-		AdCourse course = plannerService.findCourseByCode("DBA/YEAR3/SEMESTER6/Module 14");
+	     faculty = plannerService.findFacultyByCode("A10");
+	     Assert.notNull(faculty, "Faculty Must Not NULL");
+		 course = plannerService.findCourseByCode("Module14");
+		 Assert.notNull(course, "Course Must Not NULL");
 		
 		course.setFaculty(faculty);
-		course.setCode("DBA/YEAR3/SEMESTER6/Module 145");
+		course.setCode("DBA/YEAR3/SEMESTER6/Module 14");
 		course.setCredit(5);
 		course.setClassification(AdAcademicClassification.LEVEL_DOCTORATE);
-		course.setTitle("SAM RESEARCH");
-		course.setTitleEn("SAM RESEARCH");
-		course.setTitleMs("KAJIAN SAM");
+		course.setTitle("Dissertation ( Part 3.1)");
+		course.setTitleEn("Dissertation ( Part 3.1)");
+		course.setTitleMs("Dissertation ( Part 3.1)");
 		
 		plannerService.updateCourse(faculty, course);
 		
@@ -82,6 +98,7 @@ public class WhenAdminUpdateStudentResearchTitle extends Stage<WhenAdminUpdateSt
 		LOG.debug("Course Detail :{}", course.getTitleMs());
 		LOG.debug("Course Detail :{}", course.getCredit());
 		LOG.debug("Course Detail :{}", course.getClassification());
+		LOG.debug("");
 		
 		return self();
 		
