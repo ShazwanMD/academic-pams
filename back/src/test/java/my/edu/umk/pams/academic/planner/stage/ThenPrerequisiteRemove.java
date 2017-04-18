ab37390ecf652b69cb8245cb3363839fb712d976
ain <ain@10.3.32.66>
2017-04-18 12:37:38 +0800

    update then PNR_1013

--- back/src/test/java/my/edu/umk/pams/academic/planner/stage/ThenPrerequisiteRemove.java
+++ back/src/test/java/my/edu/umk/pams/academic/planner/stage/ThenPrerequisiteRemove.java
@@ -1,16 +1,41 @@
 package my.edu.umk.pams.academic.planner.stage;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
+
 import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 import com.tngtech.jgiven.integration.spring.JGivenStage;
+
+import my.edu.umk.pams.academic.planner.model.AdCourse;
+import my.edu.umk.pams.academic.planner.model.AdCoursePrerequisite;
+import my.edu.umk.pams.academic.planner.service.PlannerService;
 
 @JGivenStage
 public class ThenPrerequisiteRemove extends Stage<ThenPrerequisiteRemove> {
 
+	private static final Logger LOG = LoggerFactory.getLogger(ThenPrerequisiteRemove.class);
 
-    public ThenPrerequisiteRemove prerequite_$_for_course_$_is_removed(String prereqCode, String courseCode) {
+	@Autowired
+	private PlannerService plannerService;
 
-        //	todo (ain) The assertion(s)
+	@ExpectedScenarioState
+	private AdCourse course;
 
-        return self();
-    }
+	@ExpectedScenarioState
+	private AdCoursePrerequisite prerequisite;
+
+	public ThenPrerequisiteRemove prerequite_$_for_course_$_is_removed(String prereqCode, String courseCode) {
+
+		AdCourse course = plannerService.findCourseByCode(courseCode);
+
+		List<AdCoursePrerequisite> prerequisites = course.getPrerequisites();
+		Assert.notNull(prerequisites, "prerequisites has been deleted");
+		
+		return self();
+	}
 }
