a0f0dab75345e245908cb97d5788d03ec12894ef
ain <ain@10.3.32.66>
2017-04-20 13:39:52 +0800

    update addStage PNR_1020

---
+++ back/src/test/java/my/edu/umk/pams/academic/planner/stage/WhenBundleSubjectAdd.java
@@ -0,0 +1,89 @@
+package my.edu.umk.pams.academic.planner.stage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.ExpectedScenarioState;
+import com.tngtech.jgiven.annotation.ProvidedScenarioState;
+import com.tngtech.jgiven.integration.spring.JGivenStage;
+
+import my.edu.umk.pams.academic.planner.model.AdAcademicPeriod;
+import my.edu.umk.pams.academic.planner.model.AdBundleSubject;
+import my.edu.umk.pams.academic.planner.model.AdBundleSubjectImpl;
+import my.edu.umk.pams.academic.planner.model.AdBundleSubjectPart;
+import my.edu.umk.pams.academic.planner.model.AdBundleSubjectPartImpl;
+import my.edu.umk.pams.academic.planner.model.AdCourse;
+import my.edu.umk.pams.academic.planner.model.AdCurriculum;
+import my.edu.umk.pams.academic.planner.model.AdCurriculumImpl;
+import my.edu.umk.pams.academic.planner.model.AdFaculty;
+import my.edu.umk.pams.academic.planner.model.AdProgram;
+import my.edu.umk.pams.academic.planner.model.AdProgramLevel;
+import my.edu.umk.pams.academic.planner.model.AdSingleSubject;
+import my.edu.umk.pams.academic.planner.model.AdSingleSubjectImpl;
+import my.edu.umk.pams.academic.planner.model.AdSubjectType;
+import my.edu.umk.pams.academic.planner.service.PlannerService;
+
+@JGivenStage
+public class WhenBundleSubjectAdd extends Stage<WhenBundleSubjectAdd> {
+
+	private static final Logger LOG = LoggerFactory.getLogger(WhenBundleSubjectAdd.class);
+
+	@Autowired
+	private PlannerService plannerService;
+
+	@ExpectedScenarioState
+	private AdFaculty faculty;
+
+	@ExpectedScenarioState
+	private AdCourse course;
+
+	@ProvidedScenarioState
+	private AdCurriculum curriculum;
+
+	@ProvidedScenarioState
+	AdBundleSubject bundleSubject;
+
+	@ProvidedScenarioState
+	AdSingleSubject subject;
+
+	@ProvidedScenarioState
+	AdBundleSubjectPart part;
+
+	public WhenBundleSubjectAdd Add_bundle_subject() {
+		
+		AdBundleSubject bundleSubject = new AdBundleSubjectImpl();
+		bundleSubject.setSubjectType(AdSubjectType.ELECTIVE);
+		bundleSubject.setPeriod(AdAcademicPeriod.II);
+		plannerService.addSubject(curriculum, bundleSubject);
+
+		return self();
+	}
+
+	public WhenBundleSubjectAdd Add_bundle_subject_part() {
+
+		AdBundleSubjectPart part1 = new AdBundleSubjectPartImpl();
+		part1.setCourse(plannerService.findCourseByCode("GST2113"));
+		AdBundleSubjectPart part2 = new AdBundleSubjectPartImpl();
+		part2.setCourse(plannerService.findCourseByCode("GST2113"));
+		plannerService.addSubjectPart(bundleSubject, part1);
+		plannerService.addSubjectPart(bundleSubject, part2);
+
+		return self();
+
+	}
+
+	public WhenBundleSubjectAdd Add_single_subject() {
+
+		AdSingleSubject subject = new AdSingleSubjectImpl();
+		subject.setCourse(plannerService.findCourseByCode("GST2113"));
+		subject.setPeriod(AdAcademicPeriod.I);
+		subject.setSubjectType(AdSubjectType.CORE);
+		plannerService.addSubject(curriculum, subject);
+
+		return self();
+
+	}
+
+}
