b4a9140723a3f67228e711c7a4e186148f7838d1
zaidanawi <zaidanawi@10.3.32.177>
2017-04-23 16:47:10 +0800

    Update WhenAdminUpdateCredit

--- back/src/test/java/my/edu/umk/pams/academic/planner/stage/WhenAdminUpdateCredit.java
+++ back/src/test/java/my/edu/umk/pams/academic/planner/stage/WhenAdminUpdateCredit.java
@@ -1,4 +1,8 @@
 package my.edu.umk.pams.academic.planner.stage;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 /*
  * @author zaida
  */
@@ -7,14 +11,18 @@ import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 import com.tngtech.jgiven.annotation.ProvidedScenarioState;
 import com.tngtech.jgiven.integration.spring.JGivenStage;
+
+import io.jsonwebtoken.lang.Assert;
 import my.edu.umk.pams.academic.planner.model.AdAcademicSession;
+import my.edu.umk.pams.academic.planner.model.AdCourse;
 import my.edu.umk.pams.academic.planner.model.AdFaculty;
 import my.edu.umk.pams.academic.planner.service.PlannerService;
 
 @JGivenStage
 public class WhenAdminUpdateCredit extends Stage<WhenAdminUpdateCredit>{
 
-	
+	private static final Logger LOG = LoggerFactory.getLogger(WhenAdminUpdateCredit.class);
+			
 	@Autowired
 	private PlannerService plannerService;
 
@@ -22,9 +30,21 @@ public class WhenAdminUpdateCredit extends Stage<WhenAdminUpdateCredit>{
 	private AdAcademicSession academicSession;
 
 	@ExpectedScenarioState
-	private AdFaculty faculty;
+	private AdFaculty faculty;	
 	
-	public WhenAdminUpdateCredit Admin_update_credit_$(Integer credit) {
+	@ProvidedScenarioState
+	private AdCourse course;
+	
+
+	
+	public WhenAdminUpdateCredit Admin_update_credit() {
+		
+
+		course = plannerService.findCourseByCode("GST5013");
+		Assert.notNull(course, "course cannot be null");
+		course.setCredit(4);
+		plannerService.updateCourse(faculty, course);
+		
 		
 		return self();
 		
