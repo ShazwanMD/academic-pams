d54143f6f6a9dd8c916bdbbf365011a6b3ff044b
zaidanawi <zaidanawi@MK02PC03>
2017-05-07 10:28:35 +0800

    fix error Study_center

--- back/src/test/java/my/edu/umk/pams/academic/planner/stage/ThenStudyCenterUpdated.java
+++ back/src/test/java/my/edu/umk/pams/academic/planner/stage/ThenStudyCenterUpdated.java
@@ -4,10 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.Assert;
+
 import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 import com.tngtech.jgiven.integration.spring.JGivenStage;
-import my.edu.umk.pams.academic.common.model.AdStudyCenterCode;
+
+import my.edu.umk.pams.academic.common.model.AdStudyCenter;
 import my.edu.umk.pams.academic.common.service.CommonService;
 
 @JGivenStage
@@ -17,7 +19,7 @@ public class ThenStudyCenterUpdated extends Stage<ThenStudyCenterUpdated> {
 	private CommonService commonService;
 
 	@ExpectedScenarioState
-	private AdStudyCenterCode studyCenterCode;
+	private AdStudyCenter studyCenterCode;
 
 	private static final Logger LOG = LoggerFactory.getLogger(ThenProgramUpdated.class);
 
