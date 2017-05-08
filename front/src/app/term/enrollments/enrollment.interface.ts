2a61fafbfb43261b3b0df5dea8787749f466d4fe
norainmatjusoh <ain.mj@umk.edu.my>
2017-05-08 10:02:28 +0800

    fix error

--- front/src/app/term/enrollments/enrollment.interface.ts
+++ front/src/app/term/enrollments/enrollment.interface.ts
@@ -1,6 +1,6 @@
 import {Section} from "../sections/section.interface";
 import {Student} from "../../identity/student.interface";
-import {AcademicSession} from "../../planner/academic-sessions/academic-session.interface";
+// import {AcademicSession} from "../../planner/academic-sessions/academic-session.interface";
 export interface Enrollment {
   id: number;
   student: Student;
