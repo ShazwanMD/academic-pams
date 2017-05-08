d7754ad2cc1b04b7307d97b75814ad75bb44025b
rafizan_baharum <rafizan.baharum@canang.com.my>
2017-05-08 17:13:51 +0800

    Fix router issues

--- front/src/app/planner/courses/course.routes.ts
+++ front/src/app/planner/courses/course.routes.ts
@@ -3,12 +3,7 @@ import {Routes, RouterModule} from '@angular/router';
 import {CourseCenterPage} from "./course-center.page";
 import {CourseDetailPage} from "./course-detail.page";
 
-// Route Configuration
 export const CourseRoutes: Routes = [
   {path: 'planner/courses', component: CourseCenterPage},
-  {path: 'courses/:code', component: CourseDetailPage},
-  // {path: 'courses-create', component: CourseCreatePage},
-  // {path: 'courses-activate/:code', component: CourseActivatePage},
-  // {path: 'courses-deactivate/:code', component: CourseDeactivatePage},
-  // {path: 'courses-edit/:code', component: CourseEditPage},
+  {path: 'planner/courses/:code', component: CourseDetailPage},
 ];
