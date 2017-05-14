10d15198d251a2ef6ec1513d0d57837e138765d0
ziana@umk.edu.my <ziana@umk.edu.my>
2017-05-14 10:25:44 +0800

    section module is in progress (data still not display)

--- front/src/app/term/sections/index.ts
+++ front/src/app/term/sections/index.ts
@@ -1,4 +1,4 @@
-import '@ngrx/core/add/operator/select';
+/*import '@ngrx/core/add/operator/select';
 import {appRoutes, appRoutingProviders} from '../../app.routes';
 import {NgModule, ModuleWithProviders} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
@@ -45,5 +45,72 @@ export class SectionSubModule {
     };
   }
 }
+
+
+*/
+
+import '@ngrx/core/add/operator/select';
+import {appRoutes, appRoutingProviders} from '../../app.routes';
+import {NgModule, ModuleWithProviders} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {ReactiveFormsModule} from '@angular/forms';
+import {CovalentCoreModule} from '@covalent/core';
+
+import {TermService} from '../../../services';
+import {CommonService} from '../../../services';
+import {IdentityService} from '../../../services';
+
+import {SectionCenterPage} from "./section-center.page";
+import {SectionListComponent} from "./component/section-list.component";
+import {SectionActions} from "./section.action";
+import {EffectsModule} from "@ngrx/effects";
+import {SectionEffects} from "./section.effect";
+import {SectionDetailPage} from "./section-detail.page";
+import {SectionComponent} from "./component/section.component";
+//import {SectionEditorDialog} from "./dialog/section-editor.dialog";
+
+@NgModule({
+  imports: [
+    appRoutes,
+    BrowserModule,
+    ReactiveFormsModule,
+    CovalentCoreModule.forRoot(),
+    EffectsModule.run(SectionEffects),
+  ],
+  declarations: [
+    // page
+    SectionCenterPage,
+    SectionDetailPage,
+
+    // component
+    SectionListComponent,
+    SectionComponent,
+    
+    //dialog
+    //SectionEditorDialog,
+    
+  ],
+  exports: [],
+})
+
+export class SectionSubModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: SectionSubModule,
+      providers: [
+        appRoutingProviders,
+        TermService,
+        IdentityService,
+        CommonService,
+        SectionActions,
+      ],
+    };
+  }
+}
+
+
+
+
+
 
 
