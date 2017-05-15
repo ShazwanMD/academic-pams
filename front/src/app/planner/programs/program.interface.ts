c1e4978e2e5bfafbca7e5803c263f6b4e042215a
'norainmatjusoh' <'ain.mj@umk.edu.my'>
2017-05-15 15:07:57 +0800

    update interface

--- front/src/app/planner/programs/program.interface.ts
+++ front/src/app/planner/programs/program.interface.ts
@@ -1,7 +1,12 @@
+import { Program } from './program.interface';
+import { Faculty } from './../faculties/faculty.interface';
 import {MetaObject} from "../../core/meta-object.interface";
 export interface Program extends MetaObject{
-  code: string;
-  title:string;
-   titleMs:string;
-   titleEn:string;
+   faculty: Faculty;
+   program: Program;
+   
+  // code: string;
+  // title:string;
+  //  titleMs:string;
+  //  titleEn:string;
 }
