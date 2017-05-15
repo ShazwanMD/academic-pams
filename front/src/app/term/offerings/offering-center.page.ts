61a1f404f361ef3f741bdf8da62359f76adffe62
pjm <pjm@10.3.242.84>
2017-05-15 08:53:09 +0800

    create new offering showdialog()page (not function yet)

--- front/src/app/term/offerings/offering-center.page.ts
+++ front/src/app/term/offerings/offering-center.page.ts
@@ -1,5 +1,5 @@
 import { TermService } from './../../../services/term.service';
-import {Component, OnInit, ChangeDetectionStrategy} from '@angular/core';
+import {Component, OnInit, ChangeDetectionStrategy, ViewContainerRef} from '@angular/core';
 import {Router, ActivatedRoute} from '@angular/router';
 
 import {CommonService} from '../../../services';
@@ -8,6 +8,8 @@ import {Observable} from "rxjs";
 import {Offering} from "./offering.interface";
 import {OfferingActions} from "./offering.action";
 import {TermModuleState} from "../index";
+import {MdDialog, MdDialogConfig, MdDialogRef} from "@angular/material";
+import {OfferingApplicationTaskCreatorDialog} from "./dialog/offering-application-task-creator.dialog";
 
 @Component({
   selector: 'pams-offering-center',
@@ -17,12 +19,15 @@ export class OfferingCenterPage implements OnInit {
 
   private OFFERINGS = "termModuleState.offerings".split(".");
   private offerings$: Observable<Offering[]>;
+  private creatorDialogRef: MdDialogRef<OfferingApplicationTaskCreatorDialog>;
 
   constructor(private router: Router,
               private route: ActivatedRoute,
               private actions: OfferingActions,
               private termService: TermService,
-              private store: Store<TermModuleState>) {
+              private store: Store<TermModuleState>,
+              private vcf: ViewContainerRef,
+              private dialog: MdDialog) {
     this.offerings$ = this.store.select(...this.OFFERINGS);
   }
 
@@ -35,6 +40,22 @@ export class OfferingCenterPage implements OnInit {
     this.router.navigate(['/offerings-detail', offering.id]);
   }
 
+  showDialog(): void {
+      console.log("showDialog");
+      let config = new MdDialogConfig();
+      config.viewContainerRef = this.vcf;
+      config.role = 'dialog';
+      config.width = '50%';
+      config.height = '65%';
+      config.position = {top: '0px'};
+      this.creatorDialogRef = this.dialog.open(OfferingApplicationTaskCreatorDialog, config);
+      this.creatorDialogRef.afterClosed().subscribe(res => {
+        console.log("close dialog");
+        // load something here
+      });
+    }
+  
+  
   ngOnInit(): void {
     this.store.dispatch(this.actions.findOfferings());
   // this.offerings$ = this.termService.findOfferings();
