6f4a9d75018d6b887139330c8b550be3b04c0815
zaidanawi <zaidanawi@10.3.32.213>
2017-05-21 13:22:22 +0800

    add new button update

---
+++ front/src/app/planner/cohorts/dialog/cohort-update.dialog.ts
@@ -0,0 +1,60 @@
+import { Component, ViewContainerRef, OnInit, AfterViewInit } from '@angular/core';
+import {FormGroup, FormControl} from '@angular/forms';
+import {FormBuilder} from '@angular/forms';
+import {Router, ActivatedRoute} from '@angular/router';
+import {IdentityService} from "../../../../services/identity.service";
+import {CommonService} from "../../../../services/common.service";
+import {PlannerService} from "../../../../services/planner.service";
+import {Cohort} from "../cohort.interface";
+import {CohortActions} from "../cohort.action";
+import {MdDialogRef} from "@angular/material";
+import {PlannerModuleState} from "../../index";
+import {Store} from "@ngrx/store";
+
+
+@Component({
+  selector: 'pams-cohort-update',
+  templateUrl: './cohort-update.dialog.html',
+})
+
+export class CohortUpdateDialog implements OnInit {
+
+  private _cohort: Cohort;
+  private updateForm: FormGroup;
+  private update: boolean = false;
+
+  constructor(private router: Router,
+              private route: ActivatedRoute,
+              private formBuilder: FormBuilder,
+              private actions: CohortActions,
+              private store: Store<PlannerModuleState>,
+              private viewContainerRef: ViewContainerRef,
+              private dialog: MdDialogRef<CohortUpdateDialog >) {
+            
+  }
+
+ set cohort(value: Cohort) {
+        this._cohort = value;
+        this.update = true;
+  }
+
+     ngOnInit(): void {
+      this.updateForm = this.formBuilder.group(<Cohort>{
+       id: null,
+       code: '',
+       title: '',
+       titleMs: '',
+       titleEn: '',
+     });
+     
+       if (this.update) this.updateForm.patchValue(this._cohort);
+  }
+     
+      save(code: Cohort, isValid: boolean) {
+        if (!code.id) this.store.dispatch(this.actions.saveCohort(code));
+        else  this.store.dispatch(this.actions.updateCohort(code));
+        this.dialog.close();
+   
+   }
+}
+
