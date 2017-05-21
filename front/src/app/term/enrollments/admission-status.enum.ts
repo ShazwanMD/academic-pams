7ee970b71537b4da9b82d6dd0a5cf7fe758646e5
asyikinresli <asyikinresli@10.3.32.62>
2017-05-21 15:38:01 +0800

    add enrollment status and standing enum data

--- front/src/app/term/enrollments/admission-status.enum.ts
+++
@@ -1,6 +0,0 @@
-export enum AdmissionStatus {
-    ADMITTED,//0
-    POSTPONED,
-    SUSPENDED,
-    CANCELLED
-}
---
+++ front/src/app/term/enrollments/enrollment-standing.enum.ts
@@ -0,0 +1,13 @@
+export enum EnrollmentStanding {
+   /* ADMITTED,
+    POSTPONED,
+    SUSPENDED,
+    CANCELLED */
+    
+    
+    UM, // DS - daftar semula bila kursus gagal
+    UG,   // DS - daftar semula untuk improve gred
+    HS,   // tak perlu ada
+    HW,      // tak perlu ada
+    MN   //tak perlu ada
+}
