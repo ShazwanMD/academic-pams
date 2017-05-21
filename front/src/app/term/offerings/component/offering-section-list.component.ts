1550e53b9014b154faafc0d9b5ec4e1cc6a56efe
asyikinresli <asyikinresli@10.3.32.62>
2017-05-21 14:58:08 +0800

    list data in each components

--- front/src/app/term/offerings/component/offering-section-list.component.ts
+++ front/src/app/term/offerings/component/offering-section-list.component.ts
@@ -30,7 +30,7 @@ export class OfferingSectionListComponent {
 
   private columns: any[] = [
     {name: 'id', label: 'Id'},
-    {name: 'ordinal', label: '#'},
+    {name: 'code', label: 'Code'},
     {name: 'capacity', label: 'Capacity'},
     {name: 'appointmentCount', label: 'Appointment'},
     {name: 'enrollmentCount', label: 'Enrollment'},
