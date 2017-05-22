295ed68a8b2faf952416dd10f767847992896565
zaidanawi <zaidanawi@10.3.32.96>
2017-05-22 10:20:11 +0800

    update creator

--- front/src/app/planner/cohorts/dialog/cohort-creator.dialog.ts
+++ front/src/app/planner/cohorts/dialog/cohort-creator.dialog.ts
@@ -1,61 +1,47 @@
-import { Faculty } from './../../faculties/faculty.interface';
 import {Component, ViewContainerRef, OnInit} from '@angular/core';
 import {FormGroup, FormControl} from '@angular/forms';
 import {FormBuilder} from '@angular/forms';
-import {Router, ActivatedRoute} from '@angular/router';
-import {IdentityService} from "../../../../services/identity.service";
-import {CommonService} from "../../../../services/common.service";
 import {PlannerService} from "../../../../services/planner.service";
 import {Cohort} from "../cohort.interface";
 import {CohortActions} from "../cohort.action";;
-import {FlowState} from "../../../core/flow-state.enum";
 import {MetaState} from "../../../core/meta-state.enum";
 import {Store} from "@ngrx/store";
 import {PlannerModuleState} from "../../index";
 import {MdDialogRef} from "@angular/material";
-import { create } from '@angular/language-service/language-service';
+import {AcademicSession} from "../../../planner/academic-sessions/academic-session.interface";
+//import {Program} from './../../programs/program.interface';
 
 @Component({
-  selector: 'pams-cohort-creator',
-  templateUrl: './cohort-creator.dialog.html',
+    selector: 'pams-cohort-creator',
+    templateUrl: './cohort-creator-creator.dialog.html',
 })
 
 export class CohortCreatorDialog implements OnInit {
 
- private creatorForm: FormGroup;
-  private create: boolean = false;
-  private _cohort: Cohort;
-
-  constructor(private formBuilder: FormBuilder,
-              private store: Store<PlannerModuleState>,
-              private actions: CohortActions,
-              private dialog: MdDialogRef<CohortCreatorDialog>) {
-  }
+    private creatorForm: FormGroup;
 
 
-    set cohort(value: Cohort) {
-        this._cohort = value;
-        this.create = true;
+    constructor(private formBuilder: FormBuilder,
+        private store: Store<PlannerModuleState>,
+        private actions: CohortActions,
+        private dialog: MdDialogRef<CohortCreatorDialog>) {
     }
 
-   ngOnInit(): void {
-      this.creatorForm= this.formBuilder.group(<Cohort>{
-       id: null,
-       code: '',
-       description:'',
+    ngOnInit(): void {
+        this.creatorForm = this.formBuilder.group(<Cohort>{
+            id: null,
+            code: '',
+            description: '',
+           // program: <Program>{},
+            academicSession: <AcademicSession>{},
 
-     
-     });
+         });
+     }
 
-        if (this.create) this.creatorForm.patchValue(this._cohort);
-  }
-  
-   save(code: Cohort, isValid: boolean) {
 
-console.log(code.code);
-
-        if (!code.id) this.store.dispatch(this.actions.saveCohort(code));
-        else  this.store.dispatch(this.actions.updateCohort(code));
+    save(cohort: Cohort, isValid: boolean) {
+       this.store.dispatch(this.actions.saveCohort(cohort));
         this.dialog.close();
+    
 }
 }
