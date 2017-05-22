a984de4baa8d2c0087c5c5c746fec9dfb27589c1
zaidanawi <zaidanawi@10.3.32.96>
2017-05-22 16:16:16 +0800

    add detail cohort

--- front/src/app/planner/cohorts/cohort-detail.page.ts
+++ front/src/app/planner/cohorts/cohort-detail.page.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit, ChangeDetectionStrategy} from '@angular/core';
 import {Router, ActivatedRoute} from '@angular/router';
-
+import {CohortUpdateDialog} from './dialog/cohort-update.dialog';
 import {IdentityService} from '../../../services';
 import {CommonService} from '../../../services';
 import {Store} from "@ngrx/store";
@@ -8,6 +8,9 @@ import {Observable} from "rxjs";
 import {Cohort} from "./cohort.interface";
 import {CohortActions} from "./cohort.action";
 import {PlannerModuleState} from "../index";
+import {MdDialog, MdDialogConfig, MdDialogRef} from "@angular/material";
+import {ViewContainerRef} from '@angular/core';
+
 
 @Component({
   selector: 'pams-cohort-detail',
@@ -18,11 +21,14 @@ export class CohortDetailPage implements OnInit {
 
   private COHORT: string[] = "plannerModuleState.cohort".split(".");
   private cohort$: Observable<Cohort>;
+    private creatorDialogRef: MdDialogRef<CohortUpdateDialog>;
 
   constructor(private router: Router,
               private route: ActivatedRoute,
               private actions: CohortActions,
-              private store: Store<PlannerModuleState>) {
+              private store: Store<PlannerModuleState>,
+              private vcf: ViewContainerRef,
+              private dialog: MdDialog) {
 
     this.cohort$ = this.store.select(...this.COHORT);
   }
@@ -37,5 +43,20 @@ export class CohortDetailPage implements OnInit {
   goBack(route: string): void {
     this.router.navigate(['/cohorts']);
   }
+
+  filter(): void {
+  }
+
+    showDialog(): void {
+    console.log("showDialog");
+    let config = new MdDialogConfig();
+    config.viewContainerRef = this.vcf;
+    config.role = 'dialog';
+    config.width = '40%';
+    config.position = {top: '0px'};
+    this.creatorDialogRef = this.dialog.open(CohortUpdateDialog, config);
+   
+    };
 }
+
 
