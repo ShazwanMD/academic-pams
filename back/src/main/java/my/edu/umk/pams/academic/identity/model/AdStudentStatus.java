d587884d3800868eac13d4ffd41b7bcbb23f6dbe
Shazwan MD <shazwan.md@umk.edu.my>
2017-05-23 17:25:13 +0800

    add Student Status(Back)

--- back/src/main/java/my/edu/umk/pams/academic/identity/model/AdStudentStatus.java
+++ back/src/main/java/my/edu/umk/pams/academic/identity/model/AdStudentStatus.java
@@ -1,9 +1,16 @@
 package my.edu.umk.pams.academic.identity.model;
 
+import my.edu.umk.pams.academic.planner.model.AdProgramStatus;
+
 public enum AdStudentStatus {
 	MATRICULATED, //0
 	ACTIVE,		  //1
 	BARRED,		  //2
 	INACTIVE,	  //3
-	GRADUATED,	  //4
+	GRADUATED;	  //4
+	
+    public static AdStudentStatus get(int index) {
+        return values()[index];
+    }
+
 }
