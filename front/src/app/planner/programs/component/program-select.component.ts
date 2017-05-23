b8e0de03747abcf31ce87c251e03b2f5ea5e5b2a
'norainmatjusoh' <'ain.mj@umk.edu.my'>
2017-05-23 14:58:26 +0800

    update

--- front/src/app/planner/programs/component/program-select.component.ts
+++ front/src/app/planner/programs/component/program-select.component.ts
@@ -1,3 +1,4 @@
+import { ProgramStatusComponent } from './program-status.component';
 import {Component, Input, OnInit} from '@angular/core';
 import {Observable} from "rxjs";
 import {Store} from "@ngrx/store";
@@ -5,6 +6,7 @@ import {FormControl} from "@angular/forms";
 import {ProgramActions} from "../program.action";
 import {Program} from "../program.interface";
 import {PlannerModuleState} from "../../index";
+// import { ProgramStatus } from "../program-status.enum";
 
 
 @Component({
@@ -13,7 +15,9 @@ import {PlannerModuleState} from "../../index";
 })
 export class ProgramSelectComponent implements OnInit {
 
+  // private programStatus:ProgramStatus[]=<ProgramStatus[]>[];
   private PROGRAMS: string[] = "plannerModuleState.programs".split(".");
+
   private programs$: Observable<Program[]>;
   @Input() placeholder: string;
   @Input() innerFormControl: FormControl;
@@ -21,8 +25,12 @@ export class ProgramSelectComponent implements OnInit {
   constructor(private store: Store<PlannerModuleState>,
               private actions: ProgramActions) {
     this.programs$ = this.store.select(...this.PROGRAMS);
-  }
 
+    //  for (var n in ProgramStatus) {
+    //   if(typeof ProgramStatus[n] === 'string')
+    //     this.programStatus.push(ProgramStatus[n.toString()]);
+    // }
+     }
   ngOnInit() {
     this.store.dispatch(this.actions.findPrograms());
   }
