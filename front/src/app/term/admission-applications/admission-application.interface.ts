c81b10960fa49ace3f19e12c08623bb7ab69429f
asyikin.mr <asyikin.mr@umk.edu.my>
2017-05-25 02:29:07 +0800

    update page and link

--- front/src/app/term/admission-applications/admission-application.interface.ts
+++ front/src/app/term/admission-applications/admission-application.interface.ts
@@ -1,8 +1,10 @@
 import {Document} from "../../core/document.interface";
 import {AcademicSession} from "../../planner/academic-sessions/academic-session.interface";
+import {Program} from "../../planner/programs/program.interface";
 import {Admission} from "../admissions/admission.interface";
 import {Student} from "../../identity/student.interface";
 export interface AdmissionApplication extends Document {
     student: Student;
     academicSession: AcademicSession;
+    program: Program;
 }
