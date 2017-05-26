959dec6166c6ea2b326b8655d34e50889bebefbb
asyikin.mr <asyikin.mr@umk.edu.my>
2017-05-26 17:15:57 +0800

    updat page to add component

--- front/src/app/term/offerings/offering.effect.ts
+++ front/src/app/term/offerings/offering.effect.ts
@@ -1,79 +1,80 @@
-import {Injectable} from '@angular/core';
-import {Effect, Actions} from '@ngrx/effects';
-import {OfferingActions} from "./offering.action";
-import {TermService} from "../../../services/term.service";
-import {from} from "rxjs/observable/from";
-import {Store} from "@ngrx/store";
-import {TermModuleState} from "../index";
-import {Offering} from './offering.interface';
+import { Injectable } from '@angular/core';
+import { Effect, Actions } from '@ngrx/effects';
+import { OfferingActions } from "./offering.action";
+import { TermService } from "../../../services/term.service";
+import { from } from "rxjs/observable/from";
+import { Store } from "@ngrx/store";
+import { TermModuleState } from "../index";
+import { Offering } from './offering.interface';
 
 
 @Injectable()
 export class OfferingEffects {
-        
-   private OFFERING: string[] = "termModuleState.program".split(".");  
-    
-  constructor(private actions$: Actions,
-              private offeringActions: OfferingActions,
-              private termService: TermService,
-              private store$: Store<TermModuleState> ) {
-  }
 
-  @Effect() findOfferings$ = this.actions$
-    .ofType(OfferingActions.FIND_OFFERINGS)
-    .switchMap(() => this.termService.findOfferings())
-    .map(offerings => this.offeringActions.findOfferingsSuccess(offerings));
+    private OFFERING: string[] = "termModuleState.program".split( "." );
 
-  @Effect() findOfferingByCanonicalCode$ = this.actions$
-    .ofType(OfferingActions.FIND_OFFERING_BY_CANONICAL_CODE)
-    .map(action => action.payload)
-    .switchMap(canonicalCode => this.termService.findOfferingByCanonicalCode(canonicalCode))
-    .map(offering => this.offeringActions.findOfferingByCanonicalCodeSuccess(offering))
-    .mergeMap(action => from([action,
-      this.offeringActions.findSectionsByOffering(action.payload),
-      this.offeringActions.findAssessmentsByOffering(action.payload),
-      this.offeringActions.findEnrollmentsByOffering(action.payload),
-      this.offeringActions.findAppointmentsByOffering(action.payload)
-    ]));
+    constructor( private actions$: Actions,
+        private offeringActions: OfferingActions,
+        private termService: TermService,
+        private store$: Store<TermModuleState> ) {
+    }
 
-  @Effect() findSectionsByOffering$ = this.actions$
-    .ofType(OfferingActions.FIND_SECTIONS_BY_OFFERING)
-    .map(action => action.payload)
-    .switchMap(offering => this.termService.findSectionsByOffering(offering))
-    .map(sections => this.offeringActions.findSectionsByOfferingSuccess(sections));
+    @Effect() findOfferings$ = this.actions$
+        .ofType( OfferingActions.FIND_OFFERINGS )
+        .switchMap(() => this.termService.findOfferings() )
+        .map( offerings => this.offeringActions.findOfferingsSuccess( offerings ) );
 
-  @Effect() findAssessmentsByOffering$ = this.actions$
-    .ofType(OfferingActions.FIND_ASSESSMENTS_BY_OFFERING)
-    .map(action => action.payload)
-    .switchMap(offering => this.termService.findAssessmentsByOffering(offering))
-    .map(sections => this.offeringActions.findAssessmentsByOfferingSuccess(sections));
+    @Effect() findOfferingByCanonicalCode$ = this.actions$
+        .ofType( OfferingActions.FIND_OFFERING_BY_CANONICAL_CODE )
+        .map( action => action.payload )
+        .switchMap( canonicalCode => this.termService.findOfferingByCanonicalCode( canonicalCode ) )
+        .map( offering => this.offeringActions.findOfferingByCanonicalCodeSuccess( offering ) )
+        .mergeMap( action => from( [action,
+            this.offeringActions.findSectionsByOffering( action.payload ),
+            this.offeringActions.findAssessmentsByOffering( action.payload ),
+            this.offeringActions.findEnrollmentsByOffering( action.payload ),
+            this.offeringActions.findAppointmentsByOffering( action.payload )
+        ] ) );
 
-  @Effect() findEnrollmentsByOffering$ = this.actions$
-    .ofType(OfferingActions.FIND_ENROLLMENTS_BY_OFFERING)
-    .map(action => action.payload)
-    .switchMap(offering => this.termService.findEnrollmentsByOffering(offering))
-    .map(sections => this.offeringActions.findEnrollmentsByOfferingSuccess(sections));
+    @Effect() findSectionsByOffering$ = this.actions$
+        .ofType( OfferingActions.FIND_SECTIONS_BY_OFFERING )
+        .map( action => action.payload )
+        .switchMap( offering => this.termService.findSectionsByOffering( offering ) )
+        .map( sections => this.offeringActions.findSectionsByOfferingSuccess( sections ) );
 
-  @Effect() findAppointmentsByOffering$ = this.actions$
-    .ofType(OfferingActions.FIND_APPOINTMENTS_BY_OFFERING)
-    .map(action => action.payload)
-    .switchMap(offering => this.termService.findAppointmentsByOffering(offering))
-    .map(sections => this.offeringActions.findAppointmentsByOfferingSuccess(sections));
-  
-  @Effect() saveOffering$ = this.actions$
-  .ofType(OfferingActions.SAVE_OFFERING)
-  .map(action => action.payload)
-  .switchMap(offering => this.termService.saveOffering(offering))
-  .map(offering => this.offeringActions.saveOfferingSuccess(offering));
-    
+    @Effect() findAssessmentsByOffering$ = this.actions$
+        .ofType( OfferingActions.FIND_ASSESSMENTS_BY_OFFERING )
+        .map( action => action.payload )
+        .switchMap( offering => this.termService.findAssessmentsByOffering( offering ) )
+        .map( sections => this.offeringActions.findAssessmentsByOfferingSuccess( sections ) );
+
+    @Effect() findEnrollmentsByOffering$ = this.actions$
+        .ofType( OfferingActions.FIND_ENROLLMENTS_BY_OFFERING )
+        .map( action => action.payload )
+        .switchMap( offering => this.termService.findEnrollmentsByOffering( offering ) )
+        .map( sections => this.offeringActions.findEnrollmentsByOfferingSuccess( sections ) );
+
+    @Effect() findAppointmentsByOffering$ = this.actions$
+        .ofType( OfferingActions.FIND_APPOINTMENTS_BY_OFFERING )
+        .map( action => action.payload )
+        .switchMap( offering => this.termService.findAppointmentsByOffering( offering ) )
+        .map( sections => this.offeringActions.findAppointmentsByOfferingSuccess( sections ) );
+
+    @Effect() saveOffering$ = this.actions$
+        .ofType( OfferingActions.SAVE_OFFERING )
+        .map( action => action.payload )
+        .switchMap( offering => this.termService.saveOffering( offering ) )
+        .map( offering => this.offeringActions.saveOfferingSuccess( offering ) );
+
     //update offering
     @Effect() updateOffering$ = this.actions$
-    .ofType(OfferingActions.UPDATE_OFFERING)
-    .map(action => action.payload)
-    .switchMap(offering => this.termService.updateOffering(offering))
-    .map(offering => this.offeringActions.updateOfferingSuccess(offering))
-    .withLatestFrom(this.store$.select(...this.OFFERING))
-    .map(state => state[1])
-    .map((offering: Offering) => this.offeringActions.findOfferingByCanonicalCode(offering.canonicalCode));
-    
+        .ofType( OfferingActions.UPDATE_OFFERING )
+        .map( action => action.payload )
+        .switchMap( offering => this.termService.updateOffering( offering ) )
+        .map( offering => this.offeringActions.updateOfferingSuccess( offering ) )
+        .withLatestFrom( this.store$.select( ...this.OFFERING ) )
+        .map( state => state[1] )
+        .map(( offering: Offering ) => this.offeringActions.findOfferingByCanonicalCode( offering.canonicalCode ) );
+
+
 }
