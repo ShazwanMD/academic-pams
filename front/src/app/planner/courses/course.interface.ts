eba6974b71c8967d9a5fe6e815c02322020fa9b7
zaidanawi <zaida.n@umk.edu.my>
2017-05-27 14:23:27 +0800

    update interface

--- front/src/app/planner/courses/course.interface.ts
+++ front/src/app/planner/courses/course.interface.ts
@@ -1,9 +1,11 @@
 import {Faculty} from "../faculties/faculty.interface";
 import {MetaObject} from "../../core/meta-object.interface";
+import {CourseStatus} from "./course-status.enum";
 export interface Course extends MetaObject{
   code: string;
   titleMs:string;
   titleEn:string;
   credit:number;
+  status: CourseStatus;
   faculty?:Faculty; // todo
 }
