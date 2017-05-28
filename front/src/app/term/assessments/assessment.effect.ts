23517da77881c1a762d60830b4718b9d9c6f9f26
Shazwan MD <shazwan.md@umk.edu.my>
2017-05-28 11:04:43 +0800

    add UpdateAssessment and DeleteAssessment(Front)

--- front/src/app/term/assessments/assessment.effect.ts
+++ front/src/app/term/assessments/assessment.effect.ts
@@ -10,32 +10,51 @@ import { TermService } from "../../../services/term.service";
 
 @Injectable()
 export class AssessmentEffects {
-    private OFFERING: string[] = "termModuleState.offering".split(".");
+    private OFFERING: string[] = "termModuleState.offering".split( "." );
 
     constructor(
         private actions$: Actions,
         private assessmentActions: AssessmentActions,
         private offeringActions: OfferingActions,
         private store$: Store<TermModuleState>,
-        private termService: TermService) {
+        private termService: TermService ) {
     }
 
 
     @Effect() addAssessment$ =
     this.actions$
-        .ofType(AssessmentActions.ADD_ASSESSMENT)
-        .map(action => action.payload)
-        .switchMap(payload => this.termService.addAssessment(payload.offering, payload.assessment))
-        .map(message => this.assessmentActions.addAssessmentSuccess(message))
-        .withLatestFrom(this.store$.select(...this.OFFERING))
-        .map(state => state[1])
-        .map((offering: Offering) => this.offeringActions.findOfferingByCanonicalCode(offering.canonicalCode));
+        .ofType( AssessmentActions.ADD_ASSESSMENT )
+        .map( action => action.payload )
+        .switchMap( payload => this.termService.addAssessment( payload.offering, payload.assessment ) )
+        .map( message => this.assessmentActions.addAssessmentSuccess( message ) )
+        .withLatestFrom( this.store$.select( ...this.OFFERING ) )
+        .map( state => state[1] )
+        .map(( offering: Offering ) => this.offeringActions.findOfferingByCanonicalCode( offering.canonicalCode ) );
 
 
-          @Effect() findAssessmentById$ = this.actions$
-    .ofType(AssessmentActions.FIND_ASSESSMENT_BY_ID)
-    .map(action => action.payload)
-    .switchMap(id => this.termService.findAssessmentById(id))
-    .map(assessment => this.assessmentActions.findAssessmentByIdSuccess(assessment));
+    @Effect() findAssessmentById$ = this.actions$
+        .ofType( AssessmentActions.FIND_ASSESSMENT_BY_ID )
+        .map( action => action.payload )
+        .switchMap( id => this.termService.findAssessmentById( id ) )
+        .map( assessment => this.assessmentActions.findAssessmentByIdSuccess( assessment ) );
+
+    @Effect() updateAssessment$ = this.actions$
+        .ofType( AssessmentActions.UPDATE_ASSESSMENT )
+        .map( action => action.payload )
+        .switchMap( payload => this.termService.updateAssessment( payload.offering, payload.assessment ) )
+        .map( message => this.assessmentActions.updateAssessmentSuccess( message ) )
+        .withLatestFrom( this.store$.select( ...this.OFFERING ) )
+        .map( state => state[1] )
+        .map(( offering: Offering ) => this.offeringActions.findOfferingByCanonicalCode( offering.canonicalCode ) );
+
+    @Effect() deleteAssessment$ = this.actions$
+        .ofType( AssessmentActions.REMOVE_ASSESSMENT)
+        .map( action => action.payload )
+        .switchMap( payload => this.termService.deleteAssessment( payload.offering, payload.assessment) )
+        .map( message => this.assessmentActions.deleteAssessmentSuccess( message ) )
+        .withLatestFrom( this.store$.select( ...this.OFFERING ) )
+        .map( state => state[1] )
+        .map(( offering: Offering) => this.offeringActions.findOfferingByCanonicalCode(offering.canonicalCode));
 
 }
+
