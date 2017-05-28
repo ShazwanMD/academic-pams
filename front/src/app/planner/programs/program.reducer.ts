bbd5b09fdbca7e076c1d1500522fc2cdc9f898a4
'norainmatjusoh' <'ain.mj@umk.edu.my'>
2017-05-28 14:30:26 +0800

    update programreducer

--- front/src/app/planner/programs/program.reducer.ts
+++ front/src/app/planner/programs/program.reducer.ts
@@ -11,6 +11,20 @@ export function programReducer(state = initialState, action: Action): ProgramSta
     case ProgramActions.FIND_PROGRAM_BY_CODE_SUCCESS: {
       return action.payload;
     }
+    case ProgramActions.CREATE_PROGRAM_SUCCESS: {
+      return action.payload;
+    }
+    case ProgramActions.FIND_PROGRAM_LEVEL_BY_CODE_SUCCESS:{
+       return action.payload;
+    }
+
+     case ProgramActions.FIND_PROGRAM_BY_CODE_SUCCESS:{
+          return action.payload;
+     }
+
+      case ProgramActions.REMOVE_PROGRAM_SUCCESS:{
+         return action.payload;
+      }
 
     default: {
       return state;
