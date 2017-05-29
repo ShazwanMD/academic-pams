da16372f2b548a463f0ed37c1f683cc8ced2af71
Abiq Saidi <abiqsaidi@gmail.com>
2017-05-29 15:41:56 +0800

    update fac status and program list component usage

--- front/src/app/planner/faculties/faculty.interface.ts
+++ front/src/app/planner/faculties/faculty.interface.ts
@@ -1,7 +1,10 @@
 import {MetaObject} from "../../core/meta-object.interface";
+import { FacultyStatus } from "./faculty-status.enum";
+
 export interface Faculty extends MetaObject{
   code: string;
   name:string;
   description: string;
   prefix: string;
+  status: FacultyStatus;
 }
