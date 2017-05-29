d3fd4e004824201cbde26132c10da5a5e6cb3932
zaidanawi <zaida.n@umk.edu.my>
2017-05-29 11:26:12 +0800

    add new course

--- front/src/app/planner/courses/course.interface.ts
+++ front/src/app/planner/courses/course.interface.ts
@@ -1,6 +1,7 @@
 import {Faculty} from "../faculties/faculty.interface";
 import {MetaObject} from "../../core/meta-object.interface";
 import {CourseStatus} from "./course-status.enum";
+
 export interface Course extends MetaObject{
   code: string;
   titleMs:string;
