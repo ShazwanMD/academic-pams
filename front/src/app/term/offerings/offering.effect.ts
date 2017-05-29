17c7e171eba82b78149c8faacd37a2c05cdd817f
'norainmatjusoh' <'ain.mj@umk.edu.my'>
2017-05-29 14:40:46 +0800

    fix conflict

--- front/src/app/term/offerings/offering.effect.ts
+++ front/src/app/term/offerings/offering.effect.ts
@@ -7,7 +7,6 @@ import { Store } from "@ngrx/store";
 import { TermModuleState } from "../index";
 import { Offering } from './offering.interface';
 
-
 @Injectable()
 export class OfferingEffects {
 
@@ -76,14 +75,18 @@ export class OfferingEffects {
         .map( state => state[1] )
         .map(( offering: Offering ) => this.offeringActions.findOfferingByCanonicalCode( offering.canonicalCode ) );
     */
- //update offering
+ 
+    //update offering
     
-    @Effect() updateOfferings$ = this.actions$
+    @Effect() updateOffering$ = this.actions$
     .ofType(OfferingActions.UPDATE_OFFERING)
     .map(action => action.payload)
     .switchMap(payload => this.termService.updateOffering(payload))
     .map(message => this.offeringActions.updateOfferingSuccess(message))
     .withLatestFrom( this.store$.select( ...this.OFFERING ) )
-    .mergeMap(action => from([action, this.offeringActions.findOfferings()]));
+    .map(state => state[1])
+    //.mergeMap(action => from([action, this.offeringActions.findOfferings()]));
+    .map((offering: Offering) => this.offeringActions.findOfferingByCanonicalCode(offering.canonicalCode));
+    
 
 }
