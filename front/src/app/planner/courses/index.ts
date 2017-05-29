6337cb629b480a0bdda80169e34bd6cd1eb30711
zaidanawi <zaida.n@umk.edu.my>
2017-05-29 11:25:36 +0800

    add

--- front/src/app/planner/courses/index.ts
+++ front/src/app/planner/courses/index.ts
@@ -7,7 +7,7 @@ import {CovalentCoreModule} from '@covalent/core';
 
 import {PlannerService} from '../../../services';
 import {CommonService} from '../../../services';
-import {IdentityService} from '../../../services';
+//import {IdentityService} from '../../../services';
 
 import {CourseCenterPage} from "./course-center.page";
 import {CourseListComponent} from "./component/course-list.component";
@@ -22,6 +22,9 @@ import {CourseSelectComponent} from "./component/course-select.component";
 import {CourseActionComponent} from "./component/course-action.component";
 import {CourseStatusComponent} from "./component/course-status.component";
 import {CourseStatusSelectComponent} from "./component/course-status-select.component";
+import { IdentityModule } from "../../identity/index";
+import {IdentityService} from '../../../services';
+
 
 @NgModule({
   imports: [
@@ -30,9 +33,11 @@ import {CourseStatusSelectComponent} from "./component/course-status-select.comp
     ReactiveFormsModule,
     CovalentCoreModule.forRoot(),
     EffectsModule.run(CourseEffects),
+    IdentityModule.forRoot(),
+
   ],
   declarations: [
-    // page
+    // page v
     CourseCenterPage,
     CourseDetailPage,
 
