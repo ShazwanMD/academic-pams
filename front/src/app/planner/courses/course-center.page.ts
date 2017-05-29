6e3ce629f6dcf3bbebe74e732b0c0c1d1cec8094
zaidanawi <zaida.n@umk.edu.my>
2017-05-29 13:08:02 +0800

    edit center.ts course

--- front/src/app/planner/courses/course-center.page.ts
+++ front/src/app/planner/courses/course-center.page.ts
@@ -1,11 +1,18 @@
-import {Component, OnInit, ChangeDetectionStrategy} from '@angular/core';
+import { CourseCreatorDialog } from './dialog/course-creator.dialog';
+import { Component, OnInit, ChangeDetectionStrategy, ViewContainerRef } from '@angular/core';
 import {Router, ActivatedRoute} from '@angular/router';
-
+import {CourseEditorDialog} from './dialog/course-editor.dialog';
 import {Store} from "@ngrx/store";
 import {Observable} from "rxjs";
 import {Course} from "./course.interface";
 import {CourseActions} from "./course.action";
 import {PlannerModuleState} from "../index";
+import {CommonService} from '../../../services';
+import {PlannerService} from './../../../services/planner.service';
+import {MdDialogRef} from '@angular/material';
+import {MdDialogConfig} from '@angular/material';
+import {MdDialog} from '@angular/material';
+
 
 @Component({
   selector: 'pams-course-center',
@@ -16,16 +23,18 @@ export class CourseCenterPage implements OnInit {
 
   private COURSES: string[] = "plannerModuleState.courses".split(".");
   private courses$: Observable<Course[]>;
+      private creatorDialogRef: MdDialogRef<CourseCreatorDialog>;
+    
 
   constructor(private router: Router,
               private route: ActivatedRoute,
               private actions: CourseActions,
-              private store: Store<PlannerModuleState>) {
-
+              private store: Store<PlannerModuleState>,
+              private vcf: ViewContainerRef,
+              private dialog: MdDialog) {
+            
+        
     this.courses$ = this.store.select(...this.COURSES);
-  }
-
-  filter(): void {
   }
 
   goBack(route: string): void {
@@ -34,11 +43,34 @@ export class CourseCenterPage implements OnInit {
 
   viewCourse(course: Course) {
     console.log("course: " + course.id);
-    this.router.navigate(['/courses-detail', course.id]);
+    this.router.navigate(['/courses-detail', course.id]); 
   }
 
-  ngOnInit(): void {
+   ngOnInit(): void {
+    console.log("find course");
     this.store.dispatch(this.actions.findCourses());
+   }
+
+   createDialog(): void {
+    this.showDialog(null);
+   }
+
+ filter(): void {
+  }
+
+    private showDialog(code: Course): void {
+    console.log("save");
+    let config = new MdDialogConfig();
+    config.viewContainerRef = this.vcf;
+    config.role = 'dialog';
+    config.width = '60%';
+    config.height = '70';
+    config.position = {top: '1px'};
+    this.creatorDialogRef = this.dialog.open(CourseCreatorDialog, config);
+    this.creatorDialogRef.afterClosed().subscribe(res => {
+      console.log("close dialog");
+
+    });
   }
 }
 
