e9f6d3819d402ba9d747fddc5b3f6283088f1866
Abiq <Abiq@10.3.243.235>
2017-05-30 11:27:07 +0800

    update admn apln interface and index

--- front/src/app/term/admission-applications/admission-application.interface.ts
+++ front/src/app/term/admission-applications/admission-application.interface.ts
@@ -1,10 +1,21 @@
-import {Document} from "../../core/document.interface";
-import {AcademicSession} from "../../planner/academic-sessions/academic-session.interface";
-import {Program} from "../../planner/programs/program.interface";
-import {Admission} from "../admissions/admission.interface";
-import {Student} from "../../identity/student.interface";
-export interface AdmissionApplication extends Document {
+import { MetaObject } from './../../core/meta-object.interface';
+import { Actor } from './../../identity/actor.interface';
+import { StudyCenter } from './../../setup/study-centers/study-center.interface';
+import { AcademicSession } from "../../planner/academic-sessions/academic-session.interface";
+import { Program } from "../../planner/programs/program.interface";
+import { Student } from "../../identity/student.interface";
+
+export interface AdmissionApplication extends MetaObject{
+    referenceNo: string;
+    sourceNo: string;
+    auditNo: string;
+    description: string;
+    cancelComment: string;
+    removeComment: string;
+    actor: Actor;
     student: Student;
     academicSession: AcademicSession;
     program: Program;
+    studyCenter: StudyCenter;
 }
+
--- front/src/app/term/admission-applications/index.ts
+++ front/src/app/term/admission-applications/index.ts
@@ -1,27 +1,28 @@
 import '@ngrx/core/add/operator/select';
-import {appRoutes, appRoutingProviders} from '../../app.routes';
-import {NgModule, ModuleWithProviders} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {ReactiveFormsModule} from '@angular/forms';
-import {CovalentCoreModule} from '@covalent/core';
-import {TermService} from '../../../services';
-import {CommonService} from '../../../services';
-import {IdentityService} from '../../../services';
-import {EffectsModule} from "@ngrx/effects";
-import {AdmissionApplicationEffects} from "./admission-application.effect";
-import {AdmissionApplicationActions} from "./admission-application.action";
-import {AdmissionSubModule} from "../admissions/index";
-import {AcademicSessionSubModule} from "../../planner/academic-sessions/index";
-import {ProgramSubModule} from "../../planner/programs/index";
-import {AdmissionApplicationDraftTaskPanel} from "./panel/admission-application-draft-task.panel";
-import {AdmissionApplicationTaskCreatorDialog} from "./dialog/admission-application-task-creator.dialog";
-import {AdmissionApplicationTaskListComponent} from "./component/admission-application-task-list.component";
-import {AdmissionApplicationTaskViewPage} from "./admission-application-task-view.page";
-import {AdmissionApplicationTaskWorkflowPanel} from "./panel/admission-application-task-workflow.panel";
-import {AdmissionApplicationTaskStatusComponent} from "./component/admission-application-task-status.component";
-import {SectionSubModule} from "../sections/index";
-import {AdmissionApplicationCenterPage} from "./admission-application-center.page";
-import {IdentityModule} from "../../identity/index";
+import { appRoutes, appRoutingProviders } from '../../app.routes';
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CovalentCoreModule } from '@covalent/core';
+import { TermService } from '../../../services';
+import { CommonService } from '../../../services';
+import { IdentityService } from '../../../services';
+import { EffectsModule } from "@ngrx/effects";
+import { AdmissionApplicationEffects } from "./admission-application.effect";
+import { AdmissionApplicationActions } from "./admission-application.action";
+import { AdmissionSubModule } from "../admissions/index";
+import { AcademicSessionSubModule } from "../../planner/academic-sessions/index";
+import { SetupModule } from "./../../setup/index";
+import { ProgramSubModule } from "../../planner/programs/index";
+import { AdmissionApplicationDraftTaskPanel } from "./panel/admission-application-draft-task.panel";
+import { AdmissionApplicationTaskCreatorDialog } from "./dialog/admission-application-task-creator.dialog";
+import { AdmissionApplicationTaskListComponent } from "./component/admission-application-task-list.component";
+import { AdmissionApplicationTaskViewPage } from "./admission-application-task-view.page";
+import { AdmissionApplicationTaskWorkflowPanel } from "./panel/admission-application-task-workflow.panel";
+import { AdmissionApplicationTaskStatusComponent } from "./component/admission-application-task-status.component";
+import { SectionSubModule } from "../sections/index";
+import { AdmissionApplicationCenterPage } from "./admission-application-center.page";
+import { IdentityModule } from "../../identity/index";
 
 @NgModule({
   imports: [
@@ -33,6 +34,7 @@ import {IdentityModule} from "../../identity/index";
     AcademicSessionSubModule.forRoot(),
     ProgramSubModule.forRoot(),
     SectionSubModule.forRoot(),
+    SetupModule.forRoot(),
     IdentityModule.forRoot(),
     EffectsModule.run(AdmissionApplicationEffects),
   ],
