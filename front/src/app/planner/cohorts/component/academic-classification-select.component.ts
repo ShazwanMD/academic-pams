a3af88e70b31f0a6d89be76f89a7fe72872f3624
Shazwan MD <shazwan.md@umk.edu.my>
2017-06-01 01:14:55 +0800

    create AcademicClassificationEnum for cohort(Front)

---
+++ front/src/app/planner/cohorts/component/academic-classification-select.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from "@angular/forms";
+import { AcademicClassification } from "../academic-classification.enum";
+
+@Component({
+    selector: 'pams-academic-classification',
+    templateUrl: './academic-classification.component.html'
+})
+export class AcademicClassificationSelectComponent implements OnInit {
+
+    private academicClassifications: AcademicClassification[] = <AcademicClassification[]>[];
+    @Input() placeholder: string:
+    @Input() innerFormControl: FormControl;
+
+    constructor() {
+        for (var n in AcademicClassification) {
+            if (typeof AcademicClassification[n] === 'string')
+                this.academicClassifications.push(AcademicClassification[n.toString()]);
+        }
+    }
+
+    ngOnInit() {
+    }
+
+    selectChangeEvent(event: AcademicClassification) {
+        this.innerFormControl.setValue(event, { emitEvent: false });
+    }
+}
