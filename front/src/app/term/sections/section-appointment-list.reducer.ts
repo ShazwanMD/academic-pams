b9bcf75b125e7161193050a5044c34468b9bb11a
Nurain <Nurain@10.3.32.148>
2017-06-08 09:54:58 +0800

    update section-appointment reducer

---
+++ front/src/app/term/sections/section-appointment-list.reducer.ts
@@ -0,0 +1,19 @@
+import { SectionAppointmentListState} from './section-appointment-list.reducer';
+import { SectionActions} from './section.action';
+import {Appointment} from '../appointments/appointment.interface';
+import {Action} from '@ngrx/store';
+
+export type SectionAppointmentListState = Appointment[];
+
+const initialState: SectionAppointmentListState = <Appointment[]>[];
+
+export function sectionAppointmentListReducer(state = initialState, action: Action): SectionAppointmentListState {
+  switch (action.type) {
+    case SectionActions.FIND_APPOINTMENTS_BY_SECTION: {
+      return action.payload;
+    }
+    default: {
+      return state;
+    }
+  }
+}
