3b7f76713bcbea6910ab7ec3addd46c005fe81d0
Rafizan Baharum <rafizan.baharum@canang.com.my>
2017-06-15 02:57:14 +0800

    Clean up

--- front/src/app/planner/academic-sessions/dialog/academic-session-creator.dialog.ts
+++ front/src/app/planner/academic-sessions/dialog/academic-session-creator.dialog.ts
@@ -1,59 +1,55 @@
-import { Component, ViewContainerRef, OnInit } from '@angular/core';
+import {Component, ViewContainerRef, OnInit} from '@angular/core';
 import {FormGroup, Validators} from '@angular/forms';
-import { FormBuilder } from '@angular/forms';
-import { Store } from "@ngrx/store";
-import { MdDialogRef } from "@angular/material";
-
-import { AcademicSession } from '../academic-session.interface';
-import { PlannerModuleState } from "../../index";
-import { AcademicSessionActions } from "../academic-session.action";
-import { AcademicSemester } from '../academic-semester-type.enum';
-import { AcademicYear } from '../../academic-years/academic-year.interface';
+import {FormBuilder} from '@angular/forms';
+import {Store} from '@ngrx/store';
+import {MdDialogRef} from '@angular/material';
+import {AcademicSession} from '../academic-session.interface';
+import {PlannerModuleState} from '../../index';
+import {AcademicSessionActions} from '../academic-session.action';
+import {AcademicSemester} from '../academic-semester-type.enum';
+import {AcademicYear} from '../../academic-years/academic-year.interface';
 
 @Component({
-    selector: 'pams-academic-session-creator',
-    templateUrl: './academic-session-creator.dialog.html',
+  selector: 'pams-academic-session-creator',
+  templateUrl: './academic-session-creator.dialog.html',
 })
 
 export class AcademicSessionCreatorDialog implements OnInit {
 
-    private createForm: FormGroup;
-    private create: boolean = false;
-    private _academicSession: AcademicSession;
-    //private year: AcademicYear; 
-    private minLength = 10; // Contoh
+  private createForm: FormGroup;
+  private create: boolean = false;
+  private _academicSession: AcademicSession;
+  private minLength: number = 10; // contoh
 
-    constructor(private formBuilder: FormBuilder,
-        private viewContainerRef: ViewContainerRef,
-        private store: Store<PlannerModuleState>,
-        private actions: AcademicSessionActions,
-        private dialog: MdDialogRef<AcademicSessionCreatorDialog>) {
-    }
+  constructor(private formBuilder: FormBuilder,
+              private store: Store<PlannerModuleState>,
+              private actions: AcademicSessionActions,
+              private dialog: MdDialogRef<AcademicSessionCreatorDialog>) {
+  }
 
-    set academicSession(value: AcademicSession) {
-        this._academicSession = value;
-        this.create = true;
-    }
+  set academicSession(value: AcademicSession) {
+    this._academicSession = value;
+    this.create = true;
+  }
 
-    ngOnInit(): void {
-        this.createForm = this.formBuilder.group({
-            id: [null],
-            code: [''],
-            description: ['', Validators.minLength(this.minLength)],
-            current: [true],
-            startDate: [null, Validators.required],
-            endDate: [null, Validators.required],
-            semester: [AcademicSemester.SEMESTER_1, Validators.required],
-            year: [<AcademicYear>{}, Validators.required],
-        });
+  ngOnInit(): void {
+    this.createForm = this.formBuilder.group({
+      id: [undefined],
+      code: [''],
+      description: ['', Validators.minLength(this.minLength)],
+      current: [true],
+      startDate: [undefined, Validators.required],
+      endDate: [undefined, Validators.required],
+      semester: [AcademicSemester.SEMESTER_1, Validators.required],
+      year: [<AcademicYear>{}, Validators.required],
+    });
 
-        if (this.create) this.createForm.patchValue(this._academicSession);
-    }
+    if (this.create) this.createForm.patchValue(this._academicSession);
+  }
 
-    save() {
-       let academicSession = this.createForm.value;
-       if (!academicSession.id) this.store.dispatch (this.actions.saveAcademicSession(academicSession));
-      else this.store.dispatch(this.actions.updateAcademicSession(academicSession));
-        this.dialog.close();
-    }
+  save(academicSession: AcademicSession): void {
+    if (!academicSession.id) this.store.dispatch(this.actions.saveAcademicSession(academicSession));
+    else this.store.dispatch(this.actions.updateAcademicSession(academicSession));
+    this.dialog.close();
+  }
 }
--- front/src/app/planner/academic-sessions/dialog/academic-session-editor.dialog.ts
+++ front/src/app/planner/academic-sessions/dialog/academic-session-editor.dialog.ts
@@ -1,60 +1,55 @@
-import { ActivatedRoute, Router } from '@angular/router';
-import { Component, ViewContainerRef, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
-import { FormBuilder } from '@angular/forms';
-import { Store } from "@ngrx/store";
-import { MdDialogRef } from "@angular/material";
+import {Component, OnInit} from '@angular/core';
+import {FormGroup, Validators} from '@angular/forms';
+import {FormBuilder} from '@angular/forms';
+import {Store} from '@ngrx/store';
+import {MdDialogRef} from '@angular/material';
 
-import { AcademicSession } from '../academic-session.interface';
-import { PlannerModuleState } from "../../index";
-import { AcademicSessionActions } from "../academic-session.action";
-import { AcademicYear } from '../../academic-years/academic-year.interface';
-import { AcademicSemester } from '../academic-semester-type.enum';
+import {AcademicSession} from '../academic-session.interface';
+import {PlannerModuleState} from '../../index';
+import {AcademicSessionActions} from '../academic-session.action';
+import {AcademicSemester} from '../academic-semester-type.enum';
 
 @Component({
-    selector: 'pams-academic-session-editor',
-    templateUrl: './academic-session-editor.dialog.html',
+  selector: 'pams-academic-session-editor',
+  templateUrl: './academic-session-editor.dialog.html',
 })
 
 export class AcademicSessionEditorDialog implements OnInit {
 
-    private editorForm: FormGroup;
-    private edit: boolean = false;
-    private _academicSession: AcademicSession;
+  private editorForm: FormGroup;
+  private edit: boolean = false;
+  private _academicSession: AcademicSession;
 
-    constructor(private formBuilder: FormBuilder,
-        private viewContainerRef: ViewContainerRef,
-        private store: Store<PlannerModuleState>,
-        private actions: AcademicSessionActions,
-        private dialog: MdDialogRef<AcademicSessionEditorDialog>) {
-    }
+  constructor(private formBuilder: FormBuilder,
+              private store: Store<PlannerModuleState>,
+              private actions: AcademicSessionActions,
+              private dialog: MdDialogRef<AcademicSessionEditorDialog>) {
+  }
 
-    set academicSession(value: AcademicSession) {
-        this._academicSession = value;
-        this.edit = true;
-    }
+  set academicSession(value: AcademicSession) {
+    this._academicSession = value;
+    this.edit = true;
+  }
 
-    ngOnInit(): void {
-        this.editorForm = this.formBuilder.group(<AcademicSession>{
-            id: null,
-            code: '',
-            description: '',
-            current: true,
-            startDate: null,
-            endDate: null,
-            semester: AcademicSemester.SEMESTER_1,
-            // year: <AcademicYear>{},
-        });
+  ngOnInit(): void {
+    this.editorForm = this.formBuilder.group({
+      id: [undefined],
+      code: [''],
+      description: ['', Validators.minLength(10)],
+      current: [true],
+      startDate: [undefined, Validators.required],
+      endDate: [undefined, Validators.required],
+      semester: [AcademicSemester.SEMESTER_1, Validators.required],
+    });
 
-        if (this.edit) this.editorForm.patchValue(this._academicSession);
-    }
+    if (this.edit) this.editorForm.patchValue(this._academicSession);
+  }
 
-    submit(academicSession: AcademicSession, isValid: boolean) {
-        console.log("updating academic session");
-        if (!academicSession.id) this.store.dispatch(this.actions.saveAcademicSession(academicSession));
-        else this.store.dispatch(this.actions.updateAcademicSession(academicSession));
-        this.dialog.close();
-    }
+  submit(academicSession: AcademicSession, isValid: boolean): void {
+    console.log('updating academic session');
+    if (!academicSession.id) this.store.dispatch(this.actions.saveAcademicSession(academicSession));
+    else this.store.dispatch(this.actions.updateAcademicSession(academicSession));
+    this.dialog.close();
+  }
 }
-
 
