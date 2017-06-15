c5b03b81e7528ee65aa4aad57538fe0ad5beb07a
PAMS05 <PAMS05@PAMS05>
2017-06-15 21:28:14 +0800

    update showDialog for admission-enrollment

--- front/src/app/term/admissions/component/admission-enrollment-info.component.ts
+++ front/src/app/term/admissions/component/admission-enrollment-info.component.ts
@@ -10,9 +10,10 @@ import { Enrollment } from "../../enrollments/enrollment.interface";
 export class AdmissionEnrollmentInfoComponent {
 
     private columns: any[] = [
-        { name: 'section.id', label: 'Section No.' },
-        { name: 'section.code', label: 'Code Section' },
-        { name: 'section.offering.titleEn', label: 'Course Title' },
+        { name: 'section.id', label: 'Section' },
+        { name: 'section.code', label: 'Code' },
+        { name: 'section.offering.titleEn', label: 'Title' },
+        { name: 'section.offering.course.credit', label: 'Credit' },
         { name: 'section.offering.program.code', label: 'Program' },
         { name: 'section.offering.course.faculty.name', label: 'Faculty' },
         { name: 'enrollmentStatus', label: 'Status' },
