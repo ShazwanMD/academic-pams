e53e11697e3bdc3e4e3196ba8e19be5ba7c2c987
'norainmatjusoh' <'ain.mj@umk.edu.my'>
2017-06-18 16:02:46 +0800

    add list component

--- front/src/app/planner/curriculums/component/curriculum-list.component.ts
+++ front/src/app/planner/curriculums/component/curriculum-list.component.ts
@@ -11,9 +11,18 @@ export class CurriculumListComponent {
   // todo: credithours, etc etc
   private columns: any[] = [
     {name: 'code', label: 'Code'},
-    {name: 'program.code', label: 'Program'},
-    {name: 'program.titleMs', label: 'Title'},
-    {name: 'program.level.code', label: 'Level'},
+    {name: 'core', label: 'Core Credit'},
+    {name: 'curriculum', label: 'Curriculum Credit'},
+    {name: 'elective', label: 'Elective Credit'},
+    // {name: 'generalCredit', label: 'General Credit'},
+    // {name: 'languageCredit', label: 'Language Credit'},
+    // {name: 'otherCredit', label: 'Other Credit'},
+    // {name: 'requiredCredit', label: 'Required Credit'},
+    // {name: 'totalCredit', label: 'Total Credit'},
+    // {name: 'ordinal', label: 'Ordinal'},
+    // {name: 'program.code', label: 'Program'},
+    // {name: 'program.titleMs', label: 'Title'},
+    // {name: 'program.level.code', label: 'Level'},
     {name: 'action', label: ''},
   ];
 
