a115cf53de3f0a9910e5670196d6c79c7175daf6
'norainmatjusoh' <'ain.mj@umk.edu.my'>
2017-06-18 15:40:22 +0800

    edit showDialog

--- front/src/app/planner/curriculums/curriculum-center.page.ts
+++ front/src/app/planner/curriculums/curriculum-center.page.ts
@@ -1,12 +1,11 @@
 import {CurriculumActions} from './curriculum.action';
 import {CurriculumEditorDialog} from './dialog/curriculum-editor.dialog';
 import {Router, ActivatedRoute} from '@angular/router';
-
 import {Component, OnInit, ViewContainerRef, ChangeDetectionStrategy} from '@angular/core';
 import {CommonService} from '../../../services';
 import {Store} from '@ngrx/store';
 import {Observable} from 'rxjs';
-import {Curriculum} from './curriculum.interface';
+import { Curriculum} from './curriculum.interface';
 import {PlannerService} from './../../../services/planner.service';
 import {PlannerModuleState} from '../index';
 import {MdDialog, MdDialogConfig, MdDialogRef} from '@angular/material';
@@ -20,7 +19,23 @@ export class CurriculumCenterPage implements OnInit {
 
   private CURRICULUMS: string[] = 'plannerModuleState.curriculums'.split('.');
   private curriculums$: Observable<Curriculum[]>;
-  private creatorDialogRef: MdDialogRef<CurriculumEditorDialog>;
+  private editorDialogRef: MdDialogRef<CurriculumEditorDialog>;
+
+    private columns: any[] = [
+   {name: 'code', label: 'Code'},
+    {name: 'core', label: 'Core Credit'},
+    {name: 'curriculum', label: 'Curriculum Credit'},
+    {name: 'elective', label: 'Elective Credit'},
+    // {name: 'general', label: 'General Credit'},
+    // {name: 'language', label: 'Language Credit'},
+    // {name: 'other', label: 'Other Credit'},
+    // {name: 'required', label: 'Required Credit'},
+    // {name: 'totalCredit', label: 'Total Credit'},
+    // {name: 'ordinal', label: 'Ordinal'},
+    {name: 'program.code', label: 'Program'},
+    {name: 'action', label: ''},
+  ];
+
 
   constructor(private router: Router,
               private route: ActivatedRoute,
@@ -31,10 +46,6 @@ export class CurriculumCenterPage implements OnInit {
     this.curriculums$ = this.store.select(...this.CURRICULUMS);
   }
 
-  goBack(route: string): void {
-    this.router.navigate(['/curriculums']);
-  }
-
   viewCurriculum(curriculum: Curriculum) {
     console.log('curriculum: ' + curriculum.id);
     this.router.navigate(['/curriculums-detail', curriculum.id]);
@@ -43,19 +54,24 @@ export class CurriculumCenterPage implements OnInit {
   filter(): void {
   }
 
-  showDialog(): void {
+  showDialog(code: Curriculum): void {
     console.log('showDialog');
     let config = new MdDialogConfig();
     config.viewContainerRef = this.vcf;
     config.role = 'dialog';
     config.width = '40%';
     config.position = {top: '0px'};
-    this.creatorDialogRef = this.dialog.open(CurriculumEditorDialog, config);
+    this.editorDialogRef = this.dialog.open(CurriculumEditorDialog, config);
+    //  if (code) this.editorDialogRef.componentInstance._curriculum = code;
     // this.creatorDialogRef.componentInstance.offering = this.offering;
-    this.creatorDialogRef.afterClosed().subscribe((res) => {
+    this.editorDialogRef.afterClosed().subscribe((res) => {
       console.log('close dialog');
       // load something here
     });
+  }
+
+  goBack(route: string): void {
+    this.router.navigate(['/curriculums']);
   }
 
   ngOnInit(): void {
