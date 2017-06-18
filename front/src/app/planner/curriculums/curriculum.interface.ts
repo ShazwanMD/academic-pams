885588a0864981b6f7d21a0a9d5165b5746351f1
'norainmatjusoh' <'ain.mj@umk.edu.my'>
2017-06-18 16:03:48 +0800

    add curriculum interface

--- front/src/app/planner/curriculums/curriculum.interface.ts
+++ front/src/app/planner/curriculums/curriculum.interface.ts
@@ -3,8 +3,20 @@ import {MetaObject} from '../../core/meta-object.interface';
 import {AcademicSession} from '../../planner/academic-sessions/academic-session.interface';
 
 export interface Curriculum extends MetaObject {
+  id: null;
   code: string;
   description: string;
+  core: string,
+  curriculum: number,
+  elective: number,
+  general: number,
+  language: number,
+  other: number,
+  required: number,
+  totalCredit: number,
+  period: number,
+  maxPeriod: number,
+  ordinal: number,
   program: Program;
   academicSession: AcademicSession;
 }
