ea0f7dbe05bb002a2830020e63fe58d0280114da
PAMS05 <PAMS05@PAMS05>
2017-06-19 13:44:37 +0800

    admission pagination

--- front/src/app/term/admissions/component/admission-list.component.ts
+++ front/src/app/term/admissions/component/admission-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, EventEmitter, Output, ChangeDetectionStrategy } from '@angular/core';
 import { Admission } from "../admission.interface";
+import { TdDataTableSortingOrder, TdDataTableService, ITdDataTableSortChangeEvent, IPageChangeEvent } from '@covalent/core';
 
 @Component({
   selector: 'pams-admission-list',
@@ -20,4 +21,48 @@ export class AdmissionListComponent {
     { name: 'status', label: 'Admission Status' },
     { name: 'action', label: '' }
   ];
+  filteredData: any[];
+  filteredTotal: number;
+  searchTerm: string = '';
+  fromRow: number = 1;
+  currentPage: number = 1;
+  pageSize: number = 5;
+  sortBy: string = 'student.identityNo';
+  sortOrder: TdDataTableSortingOrder = TdDataTableSortingOrder.Descending;
+
+  
+  constructor(private _dataTableService: TdDataTableService) {}
+  
+  ngAfterViewInit(): void {
+    this.filteredData = this.admissions;
+    this.filteredTotal = this.admissions.length;
+    this.filter();
+  }
+
+  sort(sortEvent: ITdDataTableSortChangeEvent): void {
+    this.sortBy = sortEvent.name;
+    this.sortOrder = sortEvent.order;
+    this.filter();
+  }
+
+  search(searchTerm: string): void {
+    this.searchTerm = searchTerm;
+    this.filter();
+  }
+
+  page(pagingEvent: IPageChangeEvent): void {
+    this.fromRow = pagingEvent.fromRow;
+    this.currentPage = pagingEvent.page;
+    this.pageSize = pagingEvent.pageSize;
+    this.filter();
+  }
+
+  filter(): void {
+    let newData: any[] = this.admissions;
+    newData = this._dataTableService.filterData(newData, this.searchTerm, true);
+    this.filteredTotal = newData.length;
+    newData = this._dataTableService.sortData(newData, this.sortBy, this.sortOrder);
+    newData = this._dataTableService.pageData(newData, this.fromRow, this.currentPage * this.pageSize);
+    this.filteredData = newData;
+  }
 }
