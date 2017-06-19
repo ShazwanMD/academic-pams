2002a17c0aa80478faccb12c08b7df8b3438a465
Shazwan MD <shazwan.md@umk.edu.my>
2017-06-19 11:47:50 +0800

    change data table to filtered data table

--- front/src/app/profile/component/profile-list.component.ts
+++ front/src/app/profile/component/profile-list.component.ts
@@ -1,12 +1,13 @@
-import {Component, Input, EventEmitter, Output, ChangeDetectionStrategy} from '@angular/core';
+import { Component, Input, EventEmitter, Output, ChangeDetectionStrategy, AfterViewInit } from '@angular/core';
 import {Student} from "../../identity/student.interface";
+import { TdDataTableSortingOrder, TdDataTableService, ITdDataTableSortChangeEvent, IPageChangeEvent } from '@covalent/core';
 
 @Component({
   selector: 'pams-profile-list',
   templateUrl: 'profile-list.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProfileListComponent {
+export class ProfileListComponent implements AfterViewInit {
 
   @Input() students: Student[];
   @Output() view = new EventEmitter<Student>();
@@ -17,4 +18,49 @@ export class ProfileListComponent {
     {name: 'email', label: 'EMAIL'},
     {name: 'action', label: ''}
   ];
+
+  filteredData: any[];
+  filteredTotal: number;
+  searchTerm: string = '';
+  fromRow: number = 1;
+  currentPage: number = 1;
+  pageSize: number = 5;
+  sortBy: string = 'identityNo';
+  sortOrder: TdDataTableSortingOrder = TdDataTableSortingOrder.Descending;
+
+  
+  constructor(private _dataTableService: TdDataTableService) {}
+  
+  ngAfterViewInit(): void {
+    this.filteredData = this.students;
+    this.filteredTotal = this.students.length;
+    this.filter();
+  }
+
+  sort(sortEvent: ITdDataTableSortChangeEvent): void {
+    this.sortBy = sortEvent.name;
+    this.sortOrder = sortEvent.order;
+    this.filter();
+  }
+
+  search(searchTerm: string): void {
+    this.searchTerm = searchTerm;
+    this.filter();
+  }
+
+  page(pagingEvent: IPageChangeEvent): void {
+    this.fromRow = pagingEvent.fromRow;
+    this.currentPage = pagingEvent.page;
+    this.pageSize = pagingEvent.pageSize;
+    this.filter();
+  }
+
+  filter(): void {
+    let newData: any[] = this.students;
+    newData = this._dataTableService.filterData(newData, this.searchTerm, true);
+    this.filteredTotal = newData.length;
+    newData = this._dataTableService.sortData(newData, this.sortBy, this.sortOrder);
+    newData = this._dataTableService.pageData(newData, this.fromRow, this.currentPage * this.pageSize);
+    this.filteredData = newData;
+  }
 }
