ac5cf10be28f36959d4400edacd1e312ab4b1818
zaidanawi <zaida.n@umk.edu.my>
2017-06-29 14:15:20 +0800

    edit routes

--- front/src/app/planner/courses/course.routes.ts
+++ front/src/app/planner/courses/course.routes.ts
@@ -5,5 +5,5 @@ import {CourseDetailPage} from "./course-detail.page";
 
 export const CourseRoutes: Routes = [
   {path: 'planner/courses', component: CourseCenterPage},
-  {path: 'planner/courses/:code', component: CourseDetailPage},
+  {path: 'planner/courses/course-detail/:code', component: CourseDetailPage},
 ];
