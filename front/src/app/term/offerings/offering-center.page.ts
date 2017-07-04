0c577abb86784b325e59e6d670fce1de145d3529
Rafizan Baharum <rafizan.baharum@canang.com.my>
2017-07-04 13:22:14 +0800

    Fix select size

--- front/src/app/term/offerings/offering-center.page.ts
+++ front/src/app/term/offerings/offering-center.page.ts
@@ -1,15 +1,14 @@
-import {TermService} from './../../../services/term.service';
+import {TermService} from '../../../services/term.service';
 import {Component, OnInit, ChangeDetectionStrategy, ViewContainerRef} from '@angular/core';
 import {Router, ActivatedRoute} from '@angular/router';
 
-import {Store} from "@ngrx/store";
-import {Observable} from "rxjs";
-import {Offering} from "./offering.interface";
-import {OfferingActions} from "./offering.action";
-import {TermModuleState} from "../index";
-import {MdDialog, MdDialogConfig, MdDialogRef} from "@angular/material";
-import {OfferingEditorDialog} from "./dialog/offering-editor.dialog";
-//import {AdmissionApplicationTaskCreatorDialog} from "../admission-applications/dialog/admission-application-task-creator.dialog";
+import {Store} from '@ngrx/store';
+import {Observable} from 'rxjs';
+import {Offering} from './offering.interface';
+import {OfferingActions} from './offering.action';
+import {TermModuleState} from '../index';
+import {MdDialog, MdDialogConfig, MdDialogRef} from '@angular/material';
+import {OfferingEditorDialog} from './dialog/offering-editor.dialog';
 
 @Component({
   selector: 'pams-offering-center',
@@ -17,7 +16,7 @@ import {OfferingEditorDialog} from "./dialog/offering-editor.dialog";
 })
 export class OfferingCenterPage implements OnInit {
 
-  private OFFERINGS: string[] = "termModuleState.offerings".split(".");
+  private OFFERINGS: string[] = 'termModuleState.offerings'.split('.');
   private offerings$: Observable<Offering[]>;
   private creatorDialogRef: MdDialogRef<OfferingEditorDialog>;
 
@@ -30,36 +29,37 @@ export class OfferingCenterPage implements OnInit {
               private dialog: MdDialog) {
     this.offerings$ = this.store.select(...this.OFFERINGS);
   }
-  
+
   goBack(route: string): void {
     this.router.navigate(['/offerings']);
   }
-  
-  viewOffering(offering: Offering) {
-    console.log("offering: " + offering.id);
+
+  viewOffering(offering: Offering): void {
+    console.log('offering: ' + offering.id);
     this.router.navigate(['/term/offerings', offering.canonicalCode]);
   }
-  
-  createDialog(): void {
-      this.showDialog(null);
-    }
 
-    private showDialog(code: Offering): void {
-    console.log("showDialog");
-    let config = new MdDialogConfig();
-    config.viewContainerRef = this.vcf;
-    config.role = 'dialog';
-    config.width = '40%';
-    config.height = '60%';
-    config.position = {top: '0px'};
-    this.creatorDialogRef = this.dialog.open(OfferingEditorDialog, config);
-    this.creatorDialogRef.afterClosed().subscribe(res => {
-    console.log("close dialog");
-      // load something here
-    });
+  createDialog(): void {
+    this.showDialog(null);
   }
-    
+
   ngOnInit(): void {
     this.store.dispatch(this.actions.findOfferings());
   }
+
+  private showDialog(code: Offering): void {
+    console.log('showDialog');
+    let config = new MdDialogConfig();
+    config.viewContainerRef = this.vcf;
+    config.role = 'dialog';
+    config.width = '60%';
+    config.height = '60%';
+    config.position = {top: '0px'};
+    this.creatorDialogRef = this.dialog.open(OfferingEditorDialog, config);
+    this.creatorDialogRef.afterClosed().subscribe((res) => {
+      console.log('close dialog');
+      // load something here
+    });
+  }
+
 }
