7684ad715ea270765b63b8c763108a0678bb5b1f
'norainmatjusoh' <'ain.mj@umk.edu.my'>
2017-07-05 14:33:58 +0800

    add subject center page.ts

---
+++ front/src/app/planner/subjects/subject-center.page.ts
@@ -0,0 +1,77 @@
+import {id} from '@swimlane/ngx-charts/release/utils';
+import {SubjectCreatorDialog} from './dialog/subject-creator.dialog';
+// import { SubjectActions } from './subject.action';
+import { Subject } from './subject.interface';
+import {Router, ActivatedRoute} from '@angular/router';
+import {Component, OnInit, ViewContainerRef} from '@angular/core';
+import {MdDialogRef} from '@angular/material';
+import {MdDialogConfig} from '@angular/material';
+import {MdDialog} from '@angular/material';
+import {IdentityService} from '../../../services';
+import {CommonService} from '../../../services';
+import {Store} from "@ngrx/store";
+import {Observable} from "rxjs";
+import {PlannerModuleState} from "../index";
+
+
+@Component({
+  selector: 'pams-subject-center',
+  templateUrl: './subject-center.page.html',
+})
+
+export class SubjectCenterPage implements OnInit {
+
+  private SUBJECTS: string[] = "plannerModuleState.subjects".split(".");
+  private subjects$: Observable<Subject[]>;
+  private creatorDialogRef: MdDialogRef<SubjectCreatorDialog>;
+
+
+  constructor(private router: Router,
+              private route: ActivatedRoute,
+              // private actions: SubjectActions,
+              private store: Store<PlannerModuleState>,
+              private vcf: ViewContainerRef,
+              private dialog: MdDialog) {
+    this.subjects$ = this.store.select(...this.SUBJECTS);
+  }
+  viewsubject(subject: Subject) {
+    console.log("subject: " + subject.id);
+    this.router.navigate(['./subjects-detail', subject.id]);
+  }
+
+
+  ngOnInit(): void {
+    console.log("find subjects");
+    // this.store.dispatch(this.actions.findSubjects());
+  }
+
+  createDialog(): void {
+    this.showDialog(null);
+  }
+
+
+  private showDialog(id: Subject): void {
+    console.log("save");
+    let config = new MdDialogConfig();
+    config.viewContainerRef = this.vcf;
+    config.role = 'dialog';
+    config.width = '60%';
+    config.height = '70';
+    config.position = {top: '1px'};
+    this.creatorDialogRef = this.dialog.open(SubjectCreatorDialog, config); if (id) this.creatorDialogRef.componentInstance.subject = id;;
+     this.creatorDialogRef.afterClosed().subscribe(res => {
+      console.log("close dialog");
+    });
+  }
+
+   goBack(route: string): void {
+    this.router.navigate(['/subjects']);
+   }
+
+  filter(): void {
+  }
+
+}
+
+
+ 
