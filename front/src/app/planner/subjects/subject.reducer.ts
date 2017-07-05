98b6289f12a9f74f70a68379ede5b4866db07078
'norainmatjusoh' <'ain.mj@umk.edu.my'>
2017-07-05 14:45:59 +0800

    add subject reducer

---
+++ front/src/app/planner/subjects/subject.reducer.ts
@@ -0,0 +1,27 @@
+// import {Action} from '@ngrx/store';
+// // import {SubjectActions} from "./subject.action";
+// import {Subject} from "./subject.interface";
+
+// export type SubjectState = Subject;
+
+// const initialState: SubjectState = <Subject>{};
+
+// export function subjectReducer(state = initialState, action: Action): SubjectState {
+//   switch (action.type) {
+//     case SubjectActions.FIND_SUBJECT_BY_CODE_SUCCESS: {
+//       return action.payload;
+//     }
+  
+//      case SubjectActions.FIND_SUBJECT_BY_CODE_SUCCESS:{
+//           return action.payload;
+//      }
+
+//       case SubjectActions.REMOVE_SUBJECT_SUCCESS:{
+//          return action.payload;
+//       }  
+      
+//     default: {
+//       return state;
+//     }
+//   }
+// }
