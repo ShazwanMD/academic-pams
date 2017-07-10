bf5546f72aa9efd1b427bbb3edca0e50b1a7a83a
Max Zain <max.zain@canang.com.my>
2017-07-10 11:46:00 +0800

    Use interceptor, share base api endpoint to reduce code

--- front/src/services/system.service.ts
+++ front/src/services/system.service.ts
@@ -1,8 +1,11 @@
 import {Injectable} from '@angular/core';
 import {HttpInterceptorService} from '@covalent/http';
+import {environment} from '../environments/environment';
 
 @Injectable()
 export class SystemService {
+
+  private system_api: string = environment.endpoint + '/api/system';
 
   constructor(private _http: HttpInterceptorService) {
   }
