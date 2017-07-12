0da105a8c344d90b8f6a68bcd94c200dfdb25cbb
PAMS08 <PAMS08@10.3.32.77>
2017-07-12 11:20:40 +0800

    update pagination - faculty

--- front/src/app/planner/faculties/component/faculty-list.component.ts
+++ front/src/app/planner/faculties/component/faculty-list.component.ts
@@ -1,19 +1,85 @@
-import {Component, Input, EventEmitter, Output, ChangeDetectionStrategy} from '@angular/core';
+import {Component, Input, EventEmitter, Output, ChangeDetectionStrategy, AfterViewInit} from '@angular/core';
 import {Faculty} from "../faculty.interface";
+import { TdDataTableSortingOrder, ITdDataTableSortChangeEvent, IPageChangeEvent, TdDataTableService } from "@covalent/core";
+import { MdSnackBar } from "@angular/material";
+import { PlannerModuleState } from './../../index';
+import { Observable } from 'rxjs';
 
-@Component({
-  selector: 'pams-faculty-list',
-  templateUrl: './faculty-list.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+
+@Component ({
+    selector: 'pams-faculty-list',
+    templateUrl: './faculty-list.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FacultyListComponent {
 
-  @Input() faculties: Faculty[];
-  @Output() view = new EventEmitter<Faculty>();
+export class FacultyListComponent implements AfterViewInit  {
 
-  private columns: any[] = [
-    {name: 'code', label: 'Code'},
-    {name: 'description', label: 'Description'},
-    {name: 'action', label: ''}
-  ];
+  
+    @Input() faculties: Faculty[];
+    @Output() view = new EventEmitter<Faculty>();
+    
+    private columns: any [] = [
+        {name: 'code', label: 'Code'},
+        {name: 'description', label: 'Description'},
+        {name: 'current', label: 'Status'},
+        {name: 'action', label: ''},
+
+    ];
+    
+    constructor(private _dataTableService: TdDataTableService,
+            private snackBar: MdSnackBar) {}
+    
+    viewFaculty(faculty:Faculty): void {
+        console.log("Emitting faculties");
+        let snackBarRef = this.snackBar.open("Viewing faculty info", "OK");
+        snackBarRef.afterDismissed().subscribe(() => {
+        this.view.emit(faculty);
+         });
+        
+       }
+
+    filteredData: any[];
+    filteredTotal: number;
+    searchTerm: string = '';
+    fromRow: number = 1;
+    currentPage: number = 1;
+    pageSize: number = 5;
+    sortBy: string = 'code';
+    sortOrder: TdDataTableSortingOrder = TdDataTableSortingOrder.Descending;
+  
+    
+     ngAfterViewInit(): void {
+      this.filteredData = this.faculties;
+      this.filteredTotal = this.faculties.length;
+      this.filter();
+    }
+
+    sort(sortEvent: ITdDataTableSortChangeEvent): void {
+      this.sortBy = sortEvent.name;
+      this.sortOrder = sortEvent.order;
+      this.filter();
+    }
+
+    search(searchTerm: string): void {
+      this.searchTerm = searchTerm;
+      
+      this.filter();
+    }
+
+    page(pagingEvent: IPageChangeEvent): void {
+      this.fromRow = pagingEvent.fromRow;
+      this.currentPage = pagingEvent.page;
+      this.pageSize = pagingEvent.pageSize;
+      this.filter();
+    }
+
+    filter(): void {
+      let newData: any[] = this.faculties;
+      newData = this._dataTableService.filterData(newData, this.searchTerm, true);
+      this.filteredTotal = newData.length;
+      newData = this._dataTableService.sortData(newData, this.sortBy, this.sortOrder);
+      newData = this._dataTableService.pageData(newData, this.fromRow, this.currentPage * this.pageSize);
+      this.filteredData = newData;
+   }
+   
 }
