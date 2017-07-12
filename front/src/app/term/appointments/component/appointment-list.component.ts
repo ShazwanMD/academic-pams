a0b3584bedb70152ba9333c7cd34dd333c7e1199
PAMS05 <PAMS05@PAMS05>
2017-07-12 12:56:46 +0800

    pagination and searching for appointment

--- front/src/app/term/appointments/component/appointment-list.component.ts
+++ front/src/app/term/appointments/component/appointment-list.component.ts
@@ -1,12 +1,14 @@
-import {Component, Input, EventEmitter, Output, ChangeDetectionStrategy} from '@angular/core';
+import {Component, Input, EventEmitter, Output, ChangeDetectionStrategy,  AfterViewInit} from '@angular/core';
+import { TdDataTableSortingOrder, TdDataTableService, ITdDataTableSortChangeEvent, IPageChangeEvent } from '@covalent/core';
 import {Appointment} from "../appointment.interface";
+import {MdSnackBar} from "@angular/material";
 
 @Component({
   selector: 'pams-appointment-list',
   templateUrl: './appointment-list.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppointmentListComponent {
+export class AppointmentListComponent implements AfterViewInit {
 
   @Input() appointments: Appointment[];
   @Output() view: EventEmitter<Appointment> = new EventEmitter<Appointment>();
@@ -20,4 +22,58 @@ export class AppointmentListComponent {
     {name: 'section.offering.program.code', label: 'Program'},
     {name: 'action', label: ''}
   ];
+  filteredData: any[];
+  filteredTotal: number;
+  searchTerm: string = '';
+  fromRow: number = 1;
+  currentPage: number = 1;
+  pageSize: number = 5;
+  sortBy: string = 'id';
+  sortOrder: TdDataTableSortingOrder = TdDataTableSortingOrder.Descending;
+
+  
+  constructor(private _dataTableService: TdDataTableService,
+              private snackBar: MdSnackBar) {}
+  
+  ngAfterViewInit(): void {
+    this.filteredData = this.appointments;
+    this.filteredTotal = this.appointments.length;
+    this.filter();
+  }
+
+  sort(sortEvent: ITdDataTableSortChangeEvent): void {
+    this.sortBy = sortEvent.name;
+    this.sortOrder = sortEvent.order;
+    this.filter();
+  }
+
+  search(searchTerm: string): void {
+    this.searchTerm = searchTerm;
+    this.filter();
+  }
+
+  page(pagingEvent: IPageChangeEvent): void {
+    this.fromRow = pagingEvent.fromRow;
+    this.currentPage = pagingEvent.page;
+    this.pageSize = pagingEvent.pageSize;
+    this.filter();
+  }
+
+  filter(): void {
+    let newData: any[] = this.appointments;
+    newData = this._dataTableService.filterData(newData, this.searchTerm, true);
+    this.filteredTotal = newData.length;
+    newData = this._dataTableService.sortData(newData, this.sortBy, this.sortOrder);
+    newData = this._dataTableService.pageData(newData, this.fromRow, this.currentPage * this.pageSize);
+    this.filteredData = newData;
+  }
+  
+  viewAppointment(appointment: Appointment): void {
+    console.log("Emitting appointment");
+    let snackBarRef = this.snackBar.open("Viewing appointment", "OK");
+    snackBarRef.afterDismissed().subscribe(() => {
+      this.view.emit(appointment);
+    });
+  } 
 }
+
