b0c9b371229ab15eba85473ce4d3ad6ee77f6c67
PAMS05 <PAMS05@PAMS05>
2017-07-15 13:31:15 +0800

    fix error on link page appointment

--- front/src/app/term/admission-applications/admission-application.routes.ts
+++ front/src/app/term/admission-applications/admission-application.routes.ts
@@ -9,16 +9,17 @@ export const AdmissionApplicationRoutes: Routes = [
   {path: 'term/admission-applications/admission-application-center2', component: StudentAdmissionApplicationCenterPage},
   {path: 'term/admission-applications', component: AdmissionApplicationCenterPage},
   {path: 'term/admission-applications/:referenceNo', component: AdmissionApplicationDetailPage},
+  //{path: 'term/admission-applications/:referenceNo/student-admission-application-detail', component: StudentAdmissionApplicationDetailPage},
   {
     path: 'term/admission-applications/admission-application-task-detail/:taskId',
     component: AdmissionApplicationTaskDetailPage,
   },
   
-  {
+  /*{
       path: 'term/admission-applications/student-admission-application-task-detail/:taskId',
       component: StudentAdmissionApplicationTaskDetailPage,
     },
-    
+*/    
   {
       path: 'term/admission-applications/student-admission-application-task-detail/:taskId',
       component: StudentAdmissionApplicationTaskDetailPage,
