ee451b875b724373b9fdec832f533526725862d3
'norainmatjusoh' <'ain.mj@umk.edu.my'>
2017-07-18 09:56:46 +0800

    add enum subjectType

---
+++ front/src/app/planner/subjects/subject-type.enum.ts
@@ -0,0 +1,10 @@
+export enum SubjectType {
+    CORE, // teras fakulti
+    CORE_ELECTIVE, // Teras program/pengkhususan
+    ELECTIVE, // elektif/Opsyen
+    GENERAL, //Umum Universiti
+    LANGUAGE, // english
+    CURRICULUM,// kokurikulum
+    OTHERS
+
+}
