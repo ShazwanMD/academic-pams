6459b27ab2d3643bfd73238eb8033f2df3a1b910
Shazwan MD <shazwan.md@umk.edu.my>
2017-07-24 10:42:20 +0800

    update Profile, show student course info

--- front/src/app/profile/component/profile-enrollment-list.component.ts
+++ front/src/app/profile/component/profile-enrollment-list.component.ts
@@ -1,5 +1,5 @@
-import {Component, Input, EventEmitter, Output, ChangeDetectionStrategy} from '@angular/core';
-import {Enrollment} from "../../term/enrollments/enrollment.interface";
+import { Component, Input, EventEmitter, Output, ChangeDetectionStrategy } from '@angular/core';
+import { Enrollment } from "../../term/enrollments/enrollment.interface";
 
 @Component({
   selector: 'pams-profile-enrollment-list',
@@ -9,4 +9,11 @@ import {Enrollment} from "../../term/enrollments/enrollment.interface";
 export class ProfileEnrollmentListComponent {
 
   @Input() enrollments: Enrollment[];
+
+
+  private columns: any[] = [
+    { name: 'section.offering.code', label: 'Course Code' },
+    { name: 'section.offering.course.credit', label: 'Credit' },
+    { name: 'gradeCode.code', label: 'Grade' }
+    ];
 }
