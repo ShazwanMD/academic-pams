487267673fd57890b48e1b40702a584499154eb8
Rafizan Baharum <rafizan.baharum@canang.com.my>
2017-07-30 01:06:44 +0800

    Fix integration test Add integration test

--- back/src/test/java/my/edu/umk/pams/academic/workflow/FacultyAddTest.java
+++ back/src/test/java/my/edu/umk/pams/academic/workflow/FacultyAddTest.java
@@ -1,8 +1,6 @@
 
 package my.edu.umk.pams.academic.workflow;
 
-import org.activiti.engine.task.Task;
-import org.apache.commons.collections.set.SynchronizedSet;
 import org.hibernate.SessionFactory;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,25 +15,18 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.util.Assert;
 
-import java.math.BigDecimal;
-import java.util.List;
 import java.util.UUID;
 
 import my.edu.umk.pams.academic.common.service.CommonService;
 import my.edu.umk.pams.academic.config.TestAppConfiguration;
-import my.edu.umk.pams.academic.graduation.model.AdGraduationApplication;
-import my.edu.umk.pams.academic.graduation.model.AdGraduationApplicationImpl;
 import my.edu.umk.pams.academic.graduation.service.GraduationService;
-import my.edu.umk.pams.academic.identity.model.AdStudent;
 import my.edu.umk.pams.academic.identity.service.IdentityService;
-import my.edu.umk.pams.academic.planner.model.AdAcademicSession;
 import my.edu.umk.pams.academic.planner.model.AdFaculty;
 import my.edu.umk.pams.academic.planner.model.AdFacultyImpl;
+import my.edu.umk.pams.academic.planner.model.AdFacultyStatus;
 import my.edu.umk.pams.academic.planner.model.AdProgram;
 import my.edu.umk.pams.academic.planner.model.AdProgramImpl;
-import my.edu.umk.pams.academic.planner.model.AdProgramLevel;
 import my.edu.umk.pams.academic.planner.model.AdProgramStatus;
 import my.edu.umk.pams.academic.planner.service.PlannerService;
 import my.edu.umk.pams.academic.profile.service.ProfileService;
@@ -46,9 +37,9 @@ import my.edu.umk.pams.academic.workflow.service.WorkflowService;
  */
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = TestAppConfiguration.class)
-public class ProgramAddTest {
+public class FacultyAddTest {
 
-    private static final Logger LOG = LoggerFactory.getLogger(ProgramAddTest.class);
+    private static final Logger LOG = LoggerFactory.getLogger(FacultyAddTest.class);
 
     @Autowired
     private SessionFactory sessionFactory;
@@ -83,14 +74,14 @@ public class ProgramAddTest {
 
     @Test
     @Rollback
-    public void addProgram() {
-        AdFaculty faculty = plannerService.findFacultyByCode("A06");
-        AdProgram program = new AdProgramImpl();
-        program.setCode("XYZ-" + UUID.randomUUID().toString().substring(0,4));
-        program.setLevel(plannerService.findProgramLevelByCode("MASTER"));
-        program.setStatus(AdProgramStatus.ACTIVATED);
-        program.setTitleEn("TEST");
-        program.setTitleMs("TEST");
-        plannerService.addProgram(faculty, program);
+    public void addFaculty() {
+        String newCode = UUID.randomUUID().toString().substring(0, 4);
+        AdFaculty faculty = new AdFacultyImpl();
+        faculty.setCode(newCode);
+        faculty.setPrefix(newCode);
+        faculty.setName("NEW FACULTY");
+        faculty.setDescription("DESCRIPTION");
+        faculty.setStatus(AdFacultyStatus.ACTIVE);
+        plannerService.saveFaculty(faculty);
     }
 }
