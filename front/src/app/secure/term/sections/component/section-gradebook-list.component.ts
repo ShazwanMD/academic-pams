6a12ed345cd6a39c37bc9c5d098502afee44ebb5
ziana@umk.edu.my <ziana@umk.edu.my>
2017-08-22 15:05:25 +0800

    list gradebook by sections

--- front/src/app/secure/term/sections/component/section-gradebook-list.component.ts
+++ front/src/app/secure/term/sections/component/section-gradebook-list.component.ts
@@ -1,11 +1,11 @@
 import {
-  AfterViewInit,
-  ChangeDetectionStrategy,
-  Component,
-  EventEmitter,
-  Input,
-  Output,
-  ViewContainerRef
+    AfterViewInit,
+    ChangeDetectionStrategy,
+    Component,
+    EventEmitter,
+    Input,
+    Output,
+    ViewContainerRef
 } from '@angular/core';
 import {Section} from '../../../../shared/model/term/section.interface';
 import {Store} from '@ngrx/store';
@@ -16,93 +16,93 @@ import {EnrollmentEditorDialog} from '../../enrollments/dialog/enrollment-editor
 import {Gradebook} from '../../../../shared/model/term/gradebook.interface';
 import {GradebookActions} from '../../gradebooks/gradebook.action';
 import {
-  IPageChangeEvent,
-  ITdDataTableSortChangeEvent,
-  TdDataTableService,
-  TdDataTableSortingOrder
+    IPageChangeEvent,
+    ITdDataTableSortChangeEvent,
+    TdDataTableService,
+    TdDataTableSortingOrder
 } from '@covalent/core';
 
 @Component({
-  selector: 'pams-section-gradebook-list',
-  templateUrl: './section-gradebook-list.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'pams-section-gradebook-list',
+    templateUrl: './section-gradebook-list.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SectionGradebookListComponent implements AfterViewInit {
 
-  private creatorDialogRef: MdDialogRef<EnrollmentEditorDialog>;
-  private selectedRows: Gradebook[];
-  private columns: any[] = [
-    {name: 'id', label: 'Id'},
-    {name: 'assessment.description', label: 'Assessment'},
-    {name: 'assessment.weight', label: 'weight(%)'},
-    {name: 'assessment.assessmentCategory', label: 'assessmentCategory'},
-    {name: 'enrollment.admission.student.identityNo', label: 'Identity No'},
-    {name: 'enrollment.admission.student.name', label: 'Name'},
-    {name: 'score', label: 'Score'},
-    {name: 'enrollment.gradeCode.code', label: 'Grade'},
+    private creatorDialogRef: MdDialogRef<EnrollmentEditorDialog>;
+    private selectedRows: Gradebook[];
+    private columns: any[] = [
+        { name: 'id', label: 'Id' },
+        { name: 'enrollment.admission.student.name', label: 'Name' },
+        { name: 'enrollment.admission.student.identityNo', label: 'Identity No' },
+        { name: 'assessment.description', label: 'Assessment' },
+        { name: 'assessment.weight', label: 'weight(%)' },
+        { name: 'assessment.assessmentCategory', label: 'assessmentCategory' },
+        { name: 'score', label: 'Score' },
+        { name: 'enrollment.gradeCode.code', label: 'Grade' },
 
-    {name: 'action', label: ''},
-  ];
+        { name: 'action', label: '' },
+    ];
 
-  filteredData: any[];
-  filteredTotal: number;
-  searchTerm: string = '';
-  fromRow: number = 1;
-  currentPage: number = 1;
-  pageSize: number = 5;
-  sortBy: string = 'score';
-  sortOrder: TdDataTableSortingOrder = TdDataTableSortingOrder.Descending;
+    filteredData: any[];
+    filteredTotal: number;
+    searchTerm: string = '';
+    fromRow: number = 1;
+    currentPage: number = 1;
+    pageSize: number = 5;
+    sortBy: string = 'score';
+    sortOrder: TdDataTableSortingOrder = TdDataTableSortingOrder.Descending;
 
-  @Input() section: Section;
-  @Input() gradebook: Gradebook;
-  @Input() gradebooks: Gradebook[];
-  @Output() view = new EventEmitter<Gradebook>();
+    @Input() section: Section;
+    @Input() gradebook: Gradebook;
+    @Input() gradebooks: Gradebook[];
+    @Output() view = new EventEmitter<Gradebook>();
 
-  constructor(private _dataTableService: TdDataTableService,
-              private router: Router,
-              private route: ActivatedRoute,
-              private actions: GradebookActions,
-              private store: Store<TermModuleState>,
-              private vcf: ViewContainerRef,
-              private dialog: MdDialog) {
-  }
+    constructor(private _dataTableService: TdDataTableService,
+        private router: Router,
+        private route: ActivatedRoute,
+        private actions: GradebookActions,
+        private store: Store<TermModuleState>,
+        private vcf: ViewContainerRef,
+        private dialog: MdDialog) {
+    }
 
-  ngAfterViewInit(): void {
-    this.filteredData = this.gradebooks;
-    this.filteredTotal = this.gradebooks.length;
-    this.filter();
-  }
+    ngAfterViewInit(): void {
+        this.filteredData = this.gradebooks;
+        this.filteredTotal = this.gradebooks.length;
+        this.filter();
+    }
 
-  sort(sortEvent: ITdDataTableSortChangeEvent): void {
-    this.sortBy = sortEvent.name;
-    this.sortOrder = sortEvent.order;
-    this.filter();
-  }
+    sort(sortEvent: ITdDataTableSortChangeEvent): void {
+        this.sortBy = sortEvent.name;
+        this.sortOrder = sortEvent.order;
+        this.filter();
+    }
 
-  search(searchTerm: string): void {
-    this.searchTerm = searchTerm;
-    this.filter();
-  }
+    search(searchTerm: string): void {
+        this.searchTerm = searchTerm;
+        this.filter();
+    }
 
-  page(pagingEvent: IPageChangeEvent): void {
-    this.fromRow = pagingEvent.fromRow;
-    this.currentPage = pagingEvent.page;
-    this.pageSize = pagingEvent.pageSize;
-    this.filter();
-  }
+    page(pagingEvent: IPageChangeEvent): void {
+        this.fromRow = pagingEvent.fromRow;
+        this.currentPage = pagingEvent.page;
+        this.pageSize = pagingEvent.pageSize;
+        this.filter();
+    }
 
-  filter(): void {
-    let newData: any[] = this.gradebooks;
-    newData = this._dataTableService.filterData(newData, this.searchTerm, true);
-    this.filteredTotal = newData.length;
-    newData = this._dataTableService.sortData(newData, this.sortBy, this.sortOrder);
-    newData = this._dataTableService.pageData(newData, this.fromRow, this.currentPage * this.pageSize);
-    this.filteredData = newData;
-  }
+    filter(): void {
+        let newData: any[] = this.gradebooks;
+        newData = this._dataTableService.filterData(newData, this.searchTerm, true);
+        this.filteredTotal = newData.length;
+        newData = this._dataTableService.sortData(newData, this.sortBy, this.sortOrder);
+        newData = this._dataTableService.pageData(newData, this.fromRow, this.currentPage * this.pageSize);
+        this.filteredData = newData;
+    }
 
-  selectRow(gradebook: Gradebook): void {
-  }
+    selectRow(gradebook: Gradebook): void {
+    }
 
-  selectAllRows(gradebooks: Gradebook[]): void {
-  }
+    selectAllRows(gradebooks: Gradebook[]): void {
+    }
 }
