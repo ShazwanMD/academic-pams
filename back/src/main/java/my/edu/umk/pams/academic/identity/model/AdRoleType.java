af4d4c3ba33c6f08d77d65a29566b571e980ebf5
Shazwan MD <shazwan.md@umk.edu.my>
2017-08-26 11:55:20 +0800

    ADD ACL MEMBER GROUP

--- back/src/main/java/my/edu/umk/pams/academic/identity/model/AdRoleType.java
+++ back/src/main/java/my/edu/umk/pams/academic/identity/model/AdRoleType.java
@@ -9,5 +9,9 @@ public enum AdRoleType {
     ROLE_ADMINISTRATOR, // 0
     ROLE_USER,          // 1
     ROLE_GUEST,         // 2
-    ROLE_LECTURER;		// 3
+    ROLE_LECTURER,		// 3
+	ROLE_MGSEB,			// 4
+	ROLE_CPS,			// 5
+	ROLE_PTJ,			// 6
+	ROLE_FACULTY;		// 7
 }
