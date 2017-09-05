3289b2a4ae4303cdb313916621af31562cefbb15
'norainmatjusoh' <'ain.mj@umk.edu.my'>
2017-09-05 11:09:53 +0800

    Validators save curriculum

--- front/src/app/secure/planner/curriculums/dialog/curriculum-editor.dialog.ts
+++ front/src/app/secure/planner/curriculums/dialog/curriculum-editor.dialog.ts
@@ -1,71 +1,62 @@
-import {MdSnackBar} from '@angular/material/snack-bar';
-import {AcademicSession} from '../../../../shared/model/planner/academic-session.interface';
-import {Program} from '../../../../shared/model/planner/program.interface';
-import {Curriculum} from '../../../../shared/model/planner/curriculum.interface';
 import {Component, OnInit} from '@angular/core';
-import {FormGroup} from '@angular/forms';
-import {FormBuilder} from '@angular/forms';
-import {Router, ActivatedRoute} from '@angular/router';
-
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Curriculum} from '../../../../shared/model/planner/curriculum.interface';
+import {Subject} from '../../../../shared/model/planner/subject.interface';
 import {CurriculumActions} from '../curriculum.action';
-;
 import {Store} from '@ngrx/store';
 import {PlannerModuleState} from '../../index';
 import {MdDialogRef} from '@angular/material';
-
-
 @Component({
   selector: 'pams-curriculum-editor',
   templateUrl: './curriculum-editor.dialog.html',
 })
-
 export class CurriculumEditorDialog implements OnInit {
-
-    private editorForm: FormGroup;
-    private edit: boolean = false;
-    private curriculum: Curriculum;
-
+  private editorForm: FormGroup;
+  private edit: boolean = false;
+  private _curriculum: Curriculum;
+  private _subject: Subject;
   constructor(private router: Router,
               private route: ActivatedRoute,
               private formBuilder: FormBuilder,
-              private snackBar: MdSnackBar,
               private store: Store<PlannerModuleState>,
               private actions: CurriculumActions,
               private dialog: MdDialogRef<CurriculumEditorDialog>) {
   }
-
-  ngOnInit(): void {
-    this.editorForm = this.formBuilder.group(<Curriculum>{
-      id:null,
-      code:'',
-      description:'',
-      coreCredit: 0,
-      curriculumCredit: 0,
-      electiveCredit: 0,
-      generalCredit: 0,
-      languageCredit: 0,
-      otherCredit: 0,
-      requiredCredit: 0,
-      totalCredit: 0,
-      period: 0,
-      maxPeriod: 0,
-      ordinal:0,
-      program: <Program>{},
-      academicSession: <AcademicSession>{},
-    });
-
-    if (this.edit) this.editorForm.patchValue(this.curriculum);
-
+  set curriculum(value: Curriculum) {
+    this._curriculum = value;
+    this.edit = true;
   }
-
+  set subject(value: Subject) {
+    this._subject = value;
+    this.edit = true;
+  }
+  ngOnInit(): void {
+    this.editorForm = this.formBuilder.group({
+      id: [undefined],
+      code: ['', Validators.required],
+      description: [''],
+      coreCredit: ['0', Validators.required],
+      curriculumCredit: [''],
+      electiveCredit: ['0', Validators.required],
+      generalCredit:[''],
+      languageCredit: [''],
+      otherCredit: [''],
+      requiredCredit: [''],
+      totalCredit: ['0', Validators.required],
+      period: ['0', Validators.required],
+      maxPeriod: ['0', Validators.required],
+      ordinal: [''],
+      program: ['', Validators.required],
+      //academicSession: <AcademicSession>{},
+      //program: <Program>{},
+      //academicSession: <AcademicSession>{},
+    });
+    if (this.edit) this.editorForm.patchValue(this._curriculum);
+  }
   save(curriculum: Curriculum, isValid: boolean): void {
-
- if (!curriculum.id) this.store.dispatch(this.actions.saveCurriculum(curriculum));
-        else this.store.dispatch(this.actions.updateCurriculum(curriculum));
-        this.dialog.close();
-
-         let snackBarRef = this.snackBar.open( 'New assessment: ' + curriculum.code + ' has been saved', '', { duration: 3000 } );
-         snackBarRef.afterDismissed().subscribe(() => {
-       } );
+    if (!curriculum.id) this.store.dispatch(this.actions.saveCurriculum(curriculum));
+    else this.store.dispatch(this.actions.updateCurriculum(curriculum));
+    this.dialog.close();
   }
 }
