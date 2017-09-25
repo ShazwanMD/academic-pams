0a22f7051f231e53569aad8a6515ed331f0528a9
PAMS05 <PAMS05@PAMS05>
2017-09-25 17:50:18 +0800

    format identityTransformer

--- back/src/main/java/my/edu/umk/pams/academic/web/module/identity/controller/IdentityTransformer.java
+++ back/src/main/java/my/edu/umk/pams/academic/web/module/identity/controller/IdentityTransformer.java
@@ -28,7 +28,7 @@ import static java.util.stream.Collectors.toList;
  */
 @Component("identityTransformer")
 public class IdentityTransformer {
-	
+
 	@Autowired
 	private TermService termService;
 
@@ -50,66 +50,60 @@ public class IdentityTransformer {
 	@Autowired
 	private TermTransformer termTransformer;
 
-    public Staff toStaffVo(AdStaff staff) {
-        Staff vo = new Staff();
-        vo.setId(staff.getId());
-        vo.setIdentityNo(staff.getIdentityNo());
-        vo.setName(staff.getName());
-        vo.setEmail(staff.getEmail());
-        vo.setMobile(staff.getMobile());
-        vo.setPhone(staff.getPhone());
-        vo.setFax(staff.getFax());
-        vo.setTitle(staff.getTitle());
-        vo.setActorType(ActorType.get(staff.getActorType().ordinal()));
-        vo.setProgram(plannerTransformer.toProgramVo(staff.getProgram()));
-        vo.setFaculty(plannerTransformer.toFacultyVo(staff.getFaculty()));
-       
-        return vo;
-    }
+	public Staff toStaffVo(AdStaff staff) {
+		Staff vo = new Staff();
+		vo.setId(staff.getId());
+		vo.setIdentityNo(staff.getIdentityNo());
+		vo.setName(staff.getName());
+		vo.setEmail(staff.getEmail());
+		vo.setMobile(staff.getMobile());
+		vo.setPhone(staff.getPhone());
+		vo.setFax(staff.getFax());
+		vo.setTitle(staff.getTitle());
+		vo.setActorType(ActorType.get(staff.getActorType().ordinal()));
+		vo.setProgram(plannerTransformer.toProgramVo(staff.getProgram()));
+		vo.setFaculty(plannerTransformer.toFacultyVo(staff.getFaculty()));
 
-    public Student toStudentVo(AdStudent student) {
-        Student vo = new Student();
-        vo.setId(student.getId());
-        vo.setIdentityNo(student.getIdentityNo());
-        vo.setName(student.getName());
-        vo.setEmail(student.getEmail());
-        vo.setMobile(student.getMobile());
-        vo.setPhone(student.getPhone());
-        vo.setFax(student.getFax());
-        vo.setStudentStatus(StudentStatus.get(student.getStudentStatus().ordinal()));
-        vo.setBalance(student.getBalance());
-        vo.setOutstanding(student.getOutstanding());
-        vo.setMemo(student.getMemo());
-        vo.setCohort(plannerTransformer.toCohortVo(student.getCohort()));
-       
-        return vo;
-    }
+		return vo;
+	}
 
-    public List<Staff> toStaffVos(List<AdStaff> staffs) {
-        List<Staff> vos = staffs.stream()
-                .map((staff) -> toStaffVo(staff))
-                .collect(toList());
-        return vos;
-    }
+	public Student toStudentVo(AdStudent student) {
+		Student vo = new Student();
+		vo.setId(student.getId());
+		vo.setIdentityNo(student.getIdentityNo());
+		vo.setName(student.getName());
+		vo.setEmail(student.getEmail());
+		vo.setMobile(student.getMobile());
+		vo.setPhone(student.getPhone());
+		vo.setFax(student.getFax());
+		vo.setStudentStatus(StudentStatus.get(student.getStudentStatus().ordinal()));
+		vo.setBalance(student.getBalance());
+		vo.setOutstanding(student.getOutstanding());
+		vo.setMemo(student.getMemo());
+		vo.setCohort(plannerTransformer.toCohortVo(student.getCohort()));
 
+		return vo;
+	}
 
-    public List<Student> toStudentVos(List<AdStudent> students) {
-        List<Student> vos = students.stream()
-                .map((student) -> toStudentVo(student))
-                .collect(toList());
-        return vos;
-    }
+	public List<Staff> toStaffVos(List<AdStaff> staffs) {
+		List<Staff> vos = staffs.stream().map((staff) -> toStaffVo(staff)).collect(toList());
+		return vos;
+	}
 
-    public List<Actor> toActorVos(List<AdActor> actors) {
-        List<Actor> vos = actors.stream()
-                .map((actor) -> toActorVo(actor))
-                .collect(toList());
-        return vos;
-    }
+	public List<Student> toStudentVos(List<AdStudent> students) {
+		List<Student> vos = students.stream().map((student) -> toStudentVo(student)).collect(toList());
+		return vos;
+	}
 
-    public Actor toActorVo(AdActor actor) {
-        if (actor instanceof AdStaff)
-            return toStaffVo((AdStaff) actor);
-        else return null;
-    }
+	public List<Actor> toActorVos(List<AdActor> actors) {
+		List<Actor> vos = actors.stream().map((actor) -> toActorVo(actor)).collect(toList());
+		return vos;
+	}
+
+	public Actor toActorVo(AdActor actor) {
+		if (actor instanceof AdStaff)
+			return toStaffVo((AdStaff) actor);
+		else
+			return null;
+	}
 }
