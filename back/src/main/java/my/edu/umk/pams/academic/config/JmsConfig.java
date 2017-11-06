e0f377a5d8b629431c2365592144b209043b8eb8
Shazwan MD <shazwan.md@umk.edu.my>
2017-11-06 14:55:16 +0800

    update JMS Config

--- back/src/main/java/my/edu/umk/pams/academic/config/JmsConfig.java
+++ back/src/main/java/my/edu/umk/pams/academic/config/JmsConfig.java
@@ -49,7 +49,7 @@ public class JmsConfig {
         public DestinationExpressionFunction() {
             queueMap = new HashMap<>();
             queueMap.put(ProgramCodePayload.class.getName(), new ActiveMQQueue("programCodeQueue2"));
-            queueMap.put(CandidatePayload.class.getName(), new ActiveMQQueue("candidateQueue"));
+            queueMap.put(CandidatePayload.class.getName(), new ActiveMQQueue("candidateQueue3"));
             queueMap.put(FacultyCodePayload.class.getName(), new ActiveMQQueue("facultyCodeQueue2"));
             queueMap.put(AdmissionPayload.class.getName(), new ActiveMQQueue("AdmissionPayloadQueue1"));
             queueMap.put(CohortCodePayload.class.getName(), new ActiveMQQueue("CohortCodePayloadQueue"));
