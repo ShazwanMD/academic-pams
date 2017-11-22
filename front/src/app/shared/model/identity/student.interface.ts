f6c9c806506c522b3b8920cb2a177fe1914f99c5
PAMS05 <PAMS05@PAMS05>
2017-11-23 06:01:22 +0800

    fix bugs report front at studentProfile page

--- front/src/app/shared/model/identity/student.interface.ts
+++ front/src/app/shared/model/identity/student.interface.ts
@@ -1,9 +1,11 @@
 import {Actor} from './actor.interface';
 import {Cohort} from '../planner/cohort.interface';
 import {StudyMode} from '../common/study-mode.interface';
+import { Program } from "../planner/program.interface";
 export interface Student extends Actor {
   studyMode: StudyMode;
   cohort: Cohort;
+  program: Program;
   studentStatus: number;
   memo: string;
   balance: number;
